8c462aad80738e99620177788e7848fbf5ed0c4e
codeguru <codeguru@users.sourceforge.net>
2012-09-23 13:36:24 -0600

    Stub classes for BaseballCardList and BaseballCardDetails, menu item to display instance of BaseballCardsDetails Activity, images that will be used for buttons and menu items

---
+++ android/src/bbct/android/BaseballCardList.java
@@ -0,0 +1,60 @@
+/*
+ * This file is part of BBCT for Android.
+ *
+ * Copyright 2012 codeguru <codeguru@users.sourceforge.net>
+ *
+ * BBCT for Android is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BBCT for Android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package bbct.android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+/**
+ *
+ * @author codeguru <codeguru@users.sourceforge.net>
+ */
+public class BaseballCardList extends Activity {
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.card_list);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        new MenuInflater(this).inflate(R.menu.option, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add:
+                this.startActivity(new Intent(this, BaseballCardDetails.class));
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
