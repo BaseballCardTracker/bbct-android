f4c57bd5671a863c6c94d4e344ce7835de65dac4
codeguru <codeguru@users.sourceforge.net>
2012-10-01 14:07:24 -0600

    Fixed bbct.common.data.BaseballCard.equals(), bbct.common.data.BaseballCard now implements Serializable, GPL notice and JavaDocs for bbct.common.BBCTStringResources.

--- src/bbct/common/BBCTStringResources.java
+++ src/bbct/common/BBCTStringResources.java
@@ -1,20 +1,50 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * This file is part of BBCT for Android.
+ *
+ * Copyright 2012 codeguru <codeguru@users.sourceforge.net>
+ *
+ * BBCT for Android is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BBCT for Android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package bbct.common;
 
 /**
+ * String resources used in the {@link bbct.common} package.
  *
- * @author lib_lab_ref08
+ * @author codeguru <codeguru@users.sourceforge.net>
  */
 public class BBCTStringResources {
 
+    /**
+     * String resources for error messages.
+     */
     public static class ErrorResources {
 
+        /**
+         * Database initialization error message.
+         */
         public static final String DATABASE_INITIALIZATION_ERROR = "Unable to properly initialize database resources.";
+        /**
+         * Database cleanup error message.
+         */
         public static final String DATABASE_CLEANUP_ERROR = "Unable to properly cleanup database resources.";
+        /**
+         * SELECT statement error message.
+         */
         public static final String DATABASE_SELECT_ERROR = "Unable to retrieve cards from database.";
+        /**
+         * UPDATE statement error message.
+         */
         public static final String DATABASE_UPDATE_ERROR = "Unable to update baseball card data.";
 
         private ErrorResources() {
