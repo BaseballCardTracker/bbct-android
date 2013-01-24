b98996c4ab50cd9fe38976c29482f89e4b5f61db
codeguru <codeguru@users.sourceforge.net>
2013-01-24 04:59:13 -0700

    Move classes from bbct.android package to bbct.android.common package

--- android/common/main/src/bbct/android/common/BaseballCardProvider.java
+++ android/common/main/src/bbct/android/common/BaseballCardProvider.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package bbct.android;
+package bbct.android.common;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
