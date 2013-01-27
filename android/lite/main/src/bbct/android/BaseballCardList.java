ff2f7d107ce74ae563baf0cc78c36f1f81a33638
codeguru <codeguru@users.sourceforge.net>
2013-01-27 11:51:26 -0700

    Place ad banner at the top of list of baseball cards

---
+++ android/lite/main/src/bbct/android/BaseballCardList.java
@@ -0,0 +1,40 @@
+/*
+ * This file is part of BBCT for Android.
+ *
+ * Copyright 2012 codeguru <codeguru@users.sourceforge.net>
+ *
+ * BBCT for Android is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BBCT for Android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package bbct.android;
+
+import android.os.Bundle;
+import com.google.ads.AdRequest;
+import com.google.ads.AdView;
+
+/**
+ *
+ * @author codeguru <codeguru@users.sourceforge.net>
+ */
+public class BaseballCardList extends bbct.android.common.BaseballCardList {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AdView adView = (AdView) this.findViewById(R.id.ad_view);
+        AdRequest adRequest = new AdRequest();
+        adRequest.addTestDevice(AdRequest.TEST_EMULATOR);
+        adView.loadAd(adRequest);
+    }
+}
