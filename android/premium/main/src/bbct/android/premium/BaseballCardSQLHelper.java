ede301b145592577d21619d421921e33b0a8a718
codeguru <codeguru@users.sourceforge.net>
2013-02-02 23:14:58 -0700

    Add bbct.android.premium.BaseballCardSQLHelper which will import data from Lite version on first use; other minor changes

--- android/premium/main/src/bbct/android/premium/BaseballCardSQLHelper.java
+++ android/premium/main/src/bbct/android/premium/BaseballCardSQLHelper.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package android.bbct.premium;
+package bbct.android.premium;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
