95aca60ccd0e40cbad3ff1d3e03b1017b189b3d4
codeguru <codeguru@users.sourceforge.net>
2013-02-22 01:45:48 -0700

    Javadocs for bbct.android.common.BBCTTestUtil

--- android/lite/test/src/bbct/android/common/BBCTTestUtil.java
+++ android/lite/test/src/bbct/android/common/BBCTTestUtil.java
@@ -35,11 +35,24 @@ import java.util.List;
 import junit.framework.Assert;
 
 /**
+ * Utility methods used for JUnit tests on classes in Android version of BBCT.
  *
  * @author codeguru <codeguru@users.sourceforge.net>
  */
 abstract public class BBCTTestUtil {
 
+    /**
+     * Assert that the given ListView contains same data as the given list of
+     * {@link bbct.common.data.BaseballCard}s.
+     *
+     * @param inst The instrumentation for the running test case. Used to
+     * synchronize this assertion with the instrumented activity class being
+     * tested.
+     * @param expectedItems A List of the expected
+     * {@link bbct.common.data.BaseballCard} data.
+     * @param listView The List view to check for
+     * {@link bbct.common.data.BaseballCard} data.
+     */
     public static void assertListViewContainsItems(Instrumentation inst, List<BaseballCard> expectedItems, ListView listView) {
         inst.waitForIdleSync();
         Assert.assertEquals(expectedItems.size(), listView.getCount());
@@ -59,6 +72,17 @@ abstract public class BBCTTestUtil {
         }
     }
 
+    /**
+     * Test that a menu item correctly launches a child activity.
+     *
+     * @param inst The instrumentation used for this test.
+     * @param activity The activity associated with the menu being tested.
+     * @param menuId The id of the menu resource.
+     * @param childActivityClass The Class of the child activity which should be
+     * launched.
+     * @return A reference to the child activity which is launched, if the test
+     * succeeds.
+     */
     public static Activity testMenuItem(Instrumentation inst, Activity activity, int menuId, Class childActivityClass) {
         Instrumentation.ActivityMonitor monitor = new Instrumentation.ActivityMonitor(childActivityClass.getName(), null, false);
         inst.addMonitor(monitor);
@@ -73,6 +97,17 @@ abstract public class BBCTTestUtil {
         return childActivity;
     }
 
+    /**
+     * Add a card to the database using the TextViews from the given
+     * @link bbct.android.common BaseballCardDetails} activity and check that
+     * the save button can be clicked.
+     *
+     * @param test The InstrumentationTestCase object performing the test.
+     * @param cardDetails The {@link bbct.android.common.BaseballCardDetails}
+     * activity being tested.
+     * @param card The {@link bbct.common.data.BaseballCard} object holding the
+     * baseball card data to add to the database.
+     */
     public static void addCard(InstrumentationTestCase test, Activity cardDetails, BaseballCard card) {
         BBCTTestUtil.sendKeysToCardDetails(test, cardDetails, card);
 
@@ -88,6 +123,17 @@ abstract public class BBCTTestUtil {
         });
     }
 
+    /**
+     * Click the "Done" button on the given
+     * {@link bbct.android.common.BaseballCardDetails} activity and assert that
+     * the activity is finishing. This is all wrapped into a helper method
+     * because the button click must be done on the UI thread while the
+     * assertion must not.
+     *
+     * @param inst The Instrumentation instance that controls the UI thread.
+     * @param cardDetails The {@link bbctandroid.common.BaseballCardDetails}
+     * activity being tested.
+     */
     public static void clickCardDetailsDone(Instrumentation inst, Activity cardDetails) {
         final Button doneButton = (Button) cardDetails.findViewById(R.id.done_button);
 
@@ -102,21 +148,31 @@ abstract public class BBCTTestUtil {
     }
 
     /**
+     * Fills in all EditText views of the given
+     * {@link bbct.android.common.BaseballCardDetails} activity.
      *
-     * @param test
-     * @param cardDetails
-     * @param card
+     * @param test The InstrumentationTestCase object performing the test.
+     * @param cardDetails The {@link bbct.android.common.BaseballCardDetails}
+     * activity being tested.
+     * @param card The {@link bbct.common.data.BaseballCard} object holding the
+     * baseball card data to add to the database.
+     * @see sendKeysToCardDetails(InstrumentationTestCase, Activity,
+     * BaseballCard, int)
      */
     public static void sendKeysToCardDetails(InstrumentationTestCase test, Activity cardDetails, BaseballCard card) {
         BBCTTestUtil.sendKeysToCardDetails(test, cardDetails, card, ALL_FIELDS);
     }
 
     /**
+     * Fills in all EditText views, except the ones indicated, of the given
+     * {@link bbct.android.common.BaseballCardDetails} activity.
      *
-     * @param test
-     * @param cardDetails
-     * @param card
-     * @param skipFlags
+     * @param test The InstrumentationTestCase object performing the test.
+     * @param cardDetails The {@link bbct.android.common.BaseballCardDetails}
+     * activity being tested.
+     * @param card The {@link bbct.common.data.BaseballCard} object holding the
+     * baseball card data to add to the database.
+     * @param skipFlags The EditText views to skip filling in.
      */
     public static void sendKeysToCardDetails(InstrumentationTestCase test, Activity cardDetails, BaseballCard card, int skipFlags) {
         Log.d(TAG, "sendKeysToCardDetails()");
@@ -183,12 +239,54 @@ abstract public class BBCTTestUtil {
 
     private BBCTTestUtil() {
     }
+    /**
+     * Fill all fields.
+     *
+     * @see sendKeysToCardDetails(InstrumentationTestCase, Activity,
+     * BaseballCard, int)
+     */
     public static final int ALL_FIELDS = 0x00;
+    /**
+     * Skip the Card Brand field.
+     *
+     * @see sendKeysToCardDetails(InstrumentationTestCase, Activity,
+     * BaseballCard, int)
+     */
     public static final int NO_BRAND = 0x01;
+    /**
+     * Skip the Card Year field.
+     *
+     * @see sendKeysToCardDetails(InstrumentationTestCase, Activity,
+     * BaseballCard, int)
+     */
     public static final int NO_YEAR = 0x02;
+    /**
+     * Skip the Card Number field.
+     *
+     * @see sendKeysToCardDetails(InstrumentationTestCase, Activity,
+     * BaseballCard, int)
+     */
     public static final int NO_NUMBER = 0x04;
+    /**
+     * Skip the Card Value field.
+     *
+     * @see sendKeysToCardDetails(InstrumentationTestCase, Activity,
+     * BaseballCard, int)
+     */
     public static final int NO_VALUE = 0x08;
+    /**
+     * Skip the Card Count field.
+     *
+     * @see sendKeysToCardDetails(InstrumentationTestCase, Activity,
+     * BaseballCard, int)
+     */
     public static final int NO_COUNT = 0x10;
+    /**
+     * Skip the Player Name field.
+     *
+     * @see sendKeysToCardDetails(InstrumentationTestCase, Activity,
+     * BaseballCard, int)
+     */
     public static final int NO_PLAYER_NAME = 0x20;
     private static final int MENU_FLAGS = 0;
     private static final int TIME_OUT = 5 * 1000; // 5 seconds
