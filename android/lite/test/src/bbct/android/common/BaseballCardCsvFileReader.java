cff2a03913cfc9c417ad49bdd794934c7f4cb4eb
codeguru <codeguru@users.sourceforge.net>
2013-03-07 21:34:43 -0700

    Javadocs for bbct.android.common.BaseballCardCsvFileReader

--- android/lite/test/src/bbct/android/common/BaseballCardCsvFileReader.java
+++ android/lite/test/src/bbct/android/common/BaseballCardCsvFileReader.java
@@ -27,19 +27,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * This class reads baseball card data from an input stream which is formatted
+ * as comma-separated values.
  *
  * @author codeguru <codeguru@users.sourceforge.net>
  */
 public class BaseballCardCsvFileReader {
-    
+
+    /**
+     * Create a {@link BaseballCardCsvFileReader} object which reads baseball
+     * card data as comma-separated values from the given
+     * {@link java.io.InputStream}. The input may contain column headers, which
+     * will be ignored.
+     *
+     * @param in The {@link java.io.InputStream} containing the comma-separated
+     * values.
+     * @param hasColHeaders Whether or not the input contains column headers.
+     * @throws IOException If an error occurs while reading the input.
+     */
     public BaseballCardCsvFileReader(InputStream in, boolean hasColHeaders) throws IOException {
         this.in = new BufferedReader(new InputStreamReader(in));
-        
+
         if (hasColHeaders) {
             this.in.readLine();
         }
     }
-    
+
+    /**
+     * Reads baseball card data from the next line of comma-spearated values.
+     *
+     * @return A {@link BaseballCard} containing the data from the input stream.
+     * @throws IOException If an error occurs while reading the input.
+     */
     public BaseballCard getNextBaseballCard() throws IOException {
         String line = this.in.readLine();
         String[] data = line.split(",");
@@ -53,24 +72,45 @@ public class BaseballCardCsvFileReader {
 
         return new BaseballCard(brand, year, number, value, count, playerName, playerPosition);
     }
-    
+
+    /**
+     * Determine if the input stream contains more baseball card data. If the
+     * input stream is ready for an input operation, then this function returns
+     * <code>true</code>. The validity of the data is determined only after
+     * calling {@link #getNextBaseballCard() } or {@link #getAllBaseballCards()
+     * }.
+     *
+     * @return <code>true</code> if the input stream is ready for an input
+     * operation; <code>false</code>, otherwise.
+     * @throws IOException If an error occurs while reading the input.
+     */
     public boolean hasNextBaseballCard() throws IOException {
         return in.ready();
     }
-    
+
+    /**
+     * Reads all the baseball card data from the input stream.
+     *
+     * @return A list of {@link BaseballCard} objects containing all of the
+     * baseball card data from the input stream.
+     * @throws IOException If an error occurs while reading the input.
+     */
     public List<BaseballCard> getAllBaseballCards() throws IOException {
         List<BaseballCard> cards = new ArrayList<BaseballCard>();
         while (this.hasNextBaseballCard()) {
             cards.add(this.getNextBaseballCard());
         }
-        
+
         return cards;
     }
-    
+
+    /**
+     * Close the input stream.
+     *
+     * @throws IOException If an error occurs while reading the input.
+     */
     public void close() throws IOException {
         this.in.close();
     }
-    
     private BufferedReader in = null;
-    
 }
