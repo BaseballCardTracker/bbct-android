cd9b65b9fedae94bad4f0676d88357d384da9bd2
codeguru <codeguru@users.sourceforge.net>
2013-03-12 09:58:20 -0600

    BBCT Common Tests: Changed Android test package from bbct.android.tests to bbct.android.common.tests to differentiate from tests for Lite Edition; moved bbct.android.tests.BBCTTestRunner to bbct.android.common.tests.BBCTTestRunner

--- android/common/test/src/bbct/android/common/tests/BBCTTestRunner.java
+++ android/common/test/src/bbct/android/common/tests/BBCTTestRunner.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package bbct.android.tests;
+package bbct.android.common.tests;
 
 import android.test.InstrumentationTestRunner;
 import bbct.android.common.BaseballCardDetailsPartialInputTest;
