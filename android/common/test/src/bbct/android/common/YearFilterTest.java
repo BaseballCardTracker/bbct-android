33cb37fb5ade199786ba883973a954fb9de87793
codeguru <codeguru@users.sourceforge.net>
2013-03-17 13:32:38 -0600

    BBCT Common Tests: Javadocs for bbct.android.common.YearFilterTest.java

--- android/common/test/src/bbct/android/common/YearFilterTest.java
+++ android/common/test/src/bbct/android/common/YearFilterTest.java
@@ -18,19 +18,34 @@
  */
 package bbct.android.common;
 
+import android.app.Activity;
 import android.widget.EditText;
 import junit.framework.Assert;
 
 /**
+ * Tests for {@link YearFilter}.
  *
  * @author codeguru <codeguru@users.sourceforge.net>
  */
 public class YearFilterTest extends FilterActivityTest<YearFilter> {
 
+    /**
+     * Create instrumented test cases for {@link YearFilter}.
+     */
     public YearFilterTest() {
         super(YearFilter.class);
     }
 
+    /**
+     * Set up test fixture. Most of the test fixture is set up by
+     * {@link FilterActivityTest#setUp()}. This class adds a {@link EditText}
+     * view which contains the year value being edited and an
+     * <code>int</code> value for the year.
+     *
+     * @throws Exception If an error occurs while chaining to the super class.
+     *
+     * @see FilterActivityTest#setUp()
+     */
     @Override
     public void setUp() throws Exception {
         super.setUp();
@@ -40,11 +55,27 @@ public class YearFilterTest extends FilterActivityTest<YearFilter> {
         this.testYear = 1976;
     }
 
+    /**
+     * Tear down the test fixture by calling {@link Activity#finish()}.
+     *
+     * @throws Exception If an error occurs while chaining to the super class.
+     *
+     * @see FilterActivityTest#tearDown()
+     */
     @Override
     public void tearDown() throws Exception {
         super.tearDown();
     }
 
+    /**
+     * Check preconditions which must hold to guarantee the validity of all
+     * other tests. Most preconditions are checked by
+     * {@link FilterActivityTest#testPreConditions()}. In addition, this class
+     * checks that the {@link EditText} view for the year is not
+     * <code>null</code>, that it is empty, and that it has focus.
+     *
+     * @see FilterActivityTest#testPreConditions()
+     */
     @Override
     public void testPreConditions() {
         super.testPreConditions();
@@ -55,22 +86,47 @@ public class YearFilterTest extends FilterActivityTest<YearFilter> {
         Assert.assertTrue(this.yearText.hasFocus());
     }
 
+    /**
+     * Get the substring representing this {@link Activity} which should appear
+     * in the title bar.
+     *
+     * @return The substring representing this {@link Activity} which should
+     * appear in the title bar.
+     *
+     * @see FilterActivityTest#testTitle()
+     */
     @Override
     protected String getTitleSubString() {
         return this.activity.getString(R.string.year_filter_title);
     }
 
+    /**
+     * Assert that the correct error message is set in the {@link EditText} view
+     * when it is empty.
+     *
+     * @see FilterActivityTest#testOkButtonOnClickWithNoInput()}
+     */
     @Override
     protected void checkErrorMessage() {
         String expectedError = this.activity.getString(R.string.year_input_error);
         Assert.assertEquals(expectedError, this.yearText.getError());
     }
 
+    /**
+     * Set the text of the year {@link EditText} view.
+     *
+     * @see FilterActivityTest#testOkButtonOnClickWithSetInputText()
+     */
     @Override
     protected void setInputText() {
         this.yearText.setText(Integer.toString(this.testYear));
     }
 
+    /**
+     * Inject instrumented key events for the year text.
+     *
+     * @see FilterActivityTest#testOkButtonOnClickWithSendInputKeys()
+     */
     @Override
     protected void sendInputKeys() {
         this.getInstrumentation().sendStringSync(Integer.toString(this.testYear));
