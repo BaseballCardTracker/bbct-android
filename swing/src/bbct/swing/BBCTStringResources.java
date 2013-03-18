2b8e41021de0421a805fa52f5af4aee0370dce7f
codeguru <codeguru@users.sourceforge.net>
2013-03-18 11:13:27 -0600

    BBCT Swing: Update version in About panel

--- swing/src/bbct/swing/BBCTStringResources.java
+++ swing/src/bbct/swing/BBCTStringResources.java
@@ -101,7 +101,7 @@ public class BBCTStringResources {
     public static class AboutResources {
 
         public static final String ABOUT_TITLE = "Baseball Card Tracker";
-        public static final String ABOUT_VERSION = "Version 0.5.2 (beta)";
+        public static final String ABOUT_VERSION = "Version 0.5.3 (beta)";
         public static final String ABOUT_COPYRIGHT = "<html>&copy 2012 codeguru</html>";
         public static final String ABOUT_WEBSITE = "Project website: http://sf.net/p/bbct";
         public static final String ABOUT_LICENSE = "<html>This software is licensed under the GPL. Visit<br> http://www.gnu.org/licenses/ for a copy of the license.</html>";
