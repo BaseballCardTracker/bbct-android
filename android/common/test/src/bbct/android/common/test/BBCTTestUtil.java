978d7131ad843d46883e13d8f3280ea7ce303f3c
codeguru <codeguru@users.sourceforge.net>
2013-03-24 10:50:03 -0600

    BBCT Common Tests: Move classes from bbct.android.common.test.util package to bbct.android.common.test package

--- android/common/test/src/bbct/android/common/test/BBCTTestUtil.java
+++ android/common/test/src/bbct/android/common/test/BBCTTestUtil.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package bbct.android.common.test.util;
+package bbct.android.common.test;
 
 import android.app.Activity;
 import android.app.Instrumentation;
--- android/common/test/src/bbct/android/common/test/Predicate.java
+++ android/common/test/src/bbct/android/common/test/Predicate.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package bbct.android.common.test.util;
+package bbct.android.common.test;
 
 /**
  * This is a generic template which provides a tool for functional programming
