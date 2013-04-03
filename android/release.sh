b82370fc0602c06c39006da4b14299a70cbf85a0
codeguru <codeguru@users.sourceforge.net>
2013-04-03 04:31:56 -0600

    BBCT Android: Generate and check MD5 and SHA1 hashes for release APK files

--- android/release.sh
+++ android/release.sh
@@ -17,18 +17,36 @@ build_apk() {
     cd -
 }
 
+hash() {
+    local edition=$1
+    local prefix=${PREFIX}-${edition}
+
+    cd ${REL_DIR}/bin/android/${edition}
+    md5sum *.apk > ${prefix}.md5
+    echo Checking MD5...
+    md5sum -c ${prefix}.md5
+    sha1sum *.apk > ${prefix}.sha1
+    echo Checking SHA1...
+    sha1sum -c ${prefix}.sha1
+    cd -
+}
+
 if [ $# == 2 ]
 then {
     lite_version=$1
     premium_version=$2
 
-    # Build APKs
+    echo Building APKs...
     build_apk lite ${lite_version}
     build_apk premium ${premium_version}
 
-    # Package Lite Edition source
+    echo Archiving source files...
     git archive --format=zip --prefix=bbct/ -o ${SRC_REL}/bbct-lite-src.${lite_version}.zip master lite common
     git archive --format=tar --prefix=bbct/ master lite common | gzip > ${SRC_REL}/bbct-lite-src.${lite_version}.tar.gz
+
+    echo Generating hashes...
+    hash lite
+    hash premium
 }
 else {
     echo Usage: './release <lite_version> <premium_version>'
