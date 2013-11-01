9156ba23b6081fb1216529a394715d76ad3756dd
codeguru <codeguru@users.sourceforge.net>
2013-11-01 11:45:55 -0600

    Javadoc: bbct.android.common.provider.BaseballCardSQLHelper

--- android/common/lib/src/bbct/android/common/provider/BaseballCardSQLHelper.java
+++ android/common/lib/src/bbct/android/common/provider/BaseballCardSQLHelper.java
@@ -30,16 +30,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Provides helper methods to access the underlying SQLite database.
+ *
  * TODO: Write JUnit tests.
  */
 public class BaseballCardSQLHelper extends SQLiteOpenHelper {
 
+    /**
+     * Name of the SQLite database.
+     */
     public static final String DATABASE_NAME = "bbct.db";
+    /**
+     * Current schema version.
+     */
     public static final int SCHEMA_VERSION = 3;
+    /**
+     * Original schema version.
+     */
     public static final int ORIGINAL_SCHEMA = 1;
+    /**
+     * Schema version when attempting to add the team field. This version was
+     * buggy.
+     */
     public static final int BAD_TEAM_SCHEMA = 2;
+    /**
+     * Schema version which correctly added the team field.
+     */
     public static final int TEAM_SCHEMA = 3;
 
+    /**
+     * Create a new {@link BaseballCardSQLHelper} with the given Android
+     * {@link Context}.
+     *
+     * @param context
+     *            The Android {@link Context} for this {@link SQLiteOpenHelper}.
+     */
     public BaseballCardSQLHelper(Context context) {
         super(context, DATABASE_NAME, null, SCHEMA_VERSION);
 
@@ -76,16 +101,25 @@ public class BaseballCardSQLHelper extends SQLiteOpenHelper {
     }
 
     /**
-     * Insert baseball card data into a SQLite Database.
+     * Insert baseball card data into a SQLite database.
      *
-     * @param card The baseball card data to insert into the database.
+     * @param card
+     *            The baseball card data to insert into the database.
      * @return The row ID of the newly inserted row, or -1 if an error occurred.
      */
     public long insertBaseballCard(BaseballCard card) {
         return this.getWritableDatabase().insert(BaseballCardContract.TABLE_NAME, null, BaseballCardContract.getContentValues(card));
     }
 
-    public void insertAllBaseballCards(SQLiteDatabase db, List<BaseballCard> cards) {
+    /**
+     * Insert data for multiple baseball cards into a SQLite database.
+     *
+     * @param cards
+     *            The list of cards to insert into the database.
+     */
+    public void insertAllBaseballCards(List<BaseballCard> cards) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
         db.beginTransactionNonExclusive();
         try {
             for (BaseballCard card : cards) {
@@ -97,6 +131,15 @@ public class BaseballCardSQLHelper extends SQLiteOpenHelper {
         }
     }
 
+    /**
+     * Update the database with edited card data.
+     *
+     * @param oldCard
+     *            The old card data.
+     * @param newCard
+     *            The new card data.
+     * @return The number of rows updated, either 0 or 1.
+     */
     public int updateBaseballCard(BaseballCard oldCard, BaseballCard newCard) {
         String[] args = {oldCard.getBrand(), Integer.toString(oldCard.getYear()), Integer.toString(oldCard.getNumber())};
         String where = BaseballCardContract.BRAND_COL_NAME + "=? AND " + BaseballCardContract.YEAR_COL_NAME + "=? AND " + BaseballCardContract.NUMBER_COL_NAME + "=?";
@@ -104,6 +147,11 @@ public class BaseballCardSQLHelper extends SQLiteOpenHelper {
         return this.getWritableDatabase().update(BaseballCardContract.TABLE_NAME, BaseballCardContract.getContentValues(newCard), where, args);
     }
 
+    /**
+     * Get the most recently opened {@link Cursor}.
+     *
+     * @return The most recently opened {@link Cursor}.
+     */
     public Cursor getCursor() {
         Log.d(TAG, "getCursor()");
 
@@ -114,30 +162,66 @@ public class BaseballCardSQLHelper extends SQLiteOpenHelper {
         return this.currCursor;
     }
 
+    /**
+     * Open a {@link Cursor} with no filter.
+     */
     public void clearFilter() {
         Log.d(TAG, "clearFilter()");
 
         this.currCursor = this.getWritableDatabase().query(BaseballCardContract.TABLE_NAME, null, null, null, null, null, null);
     }
 
+    /**
+     * Open a {@link Cursor} which contains all baseball cards published during
+     * the given year. Call {@link #getCursor()} to obtain the opened
+     * {@link Cursor}.
+     *
+     * @param year
+     *            The card publication year to query.
+     */
     public void filterCursorByYear(int year) {
         String filter = BaseballCardContract.YEAR_COL_NAME + " = ?";
         String[] args = {Integer.toString(year)};
         this.currCursor = this.getWritableDatabase().query(BaseballCardContract.TABLE_NAME, null, filter, args, null, null, null);
     }
 
+    /**
+     * Open a {@link Cursor} which contains all baseball cards with the given
+     * card number. Call {@link #getCursor()} to obtain the opened
+     * {@link Cursor}.
+     *
+     * @param number
+     *            The card number to query.
+     */
     public void filterCursorByNumber(int number) {
         String filter = BaseballCardContract.NUMBER_COL_NAME + " = ?";
         String[] args = {Integer.toString(number)};
         this.currCursor = this.getWritableDatabase().query(BaseballCardContract.TABLE_NAME, null, filter, args, null, null, null);
     }
 
+    /**
+     * Open a {@link Cursor} which contains all baseball cards published during
+     * the given year with the given card number. Call {@link #getCursor()} to
+     * obtain the opened {@link Cursor}.
+     *
+     * @param year
+     *            The card publication year to query.
+     * @param number
+     *            The card number to query.
+     */
     public void filterCursorByYearAndNumber(int year, int number) {
         String filter = BaseballCardContract.YEAR_COL_NAME + " = ?  AND " + BaseballCardContract.NUMBER_COL_NAME + " = ?";
         String[] args = {Integer.toString(year), Integer.toString(number)};
         this.currCursor = this.getWritableDatabase().query(BaseballCardContract.TABLE_NAME, null, filter, args, null, null, null);
     }
 
+    /**
+     * Open a {@link Cursor} which contains all baseball cards for the given
+     * player. Call {@link #getCursor()} to obtain the opened {@link Cursor}.
+     *
+     * @param playerName
+     *            The player name to query.
+     */
     public void filterCursorByPlayerName(String playerName) {
         // TODO: Document wild cards in user manual?
         String filter = BaseballCardContract.PLAYER_NAME_COL_NAME + " LIKE ?";
@@ -145,6 +229,14 @@ public class BaseballCardSQLHelper extends SQLiteOpenHelper {
         this.currCursor = this.getWritableDatabase().query(BaseballCardContract.TABLE_NAME, null, filter, args, null, null, null);
     }
 
+    /**
+     * Open a {@link Cursor} which contains all baseball cards for players on
+     * the given team. Call {@link #getCursor()} to obtain the opened
+     * {@link Cursor}.
+     *
+     * @param team
+     *            The team name to query.
+     */
     public void filterCursorByTeam(String team) {
         // TODO: Document wild cards in user manual?
         String filter = BaseballCardContract.TEAM_COL_NAME + " LIKE ?";
@@ -152,10 +244,26 @@ public class BaseballCardSQLHelper extends SQLiteOpenHelper {
         this.currCursor = this.getWritableDatabase().query(BaseballCardContract.TABLE_NAME, null, filter, args, null, null, null);
     }
 
+    /**
+     * Populate a {@link BaseballCard} from the data in the current row of the
+     * current {@link Cursor}.
+     *
+     * @return A {@link BaseballCard} from the data in the current row of the
+     *         current {@link Cursor}.
+     */
     public BaseballCard getBaseballCardFromCursor() {
         return this.getBaseballCardFromCursor(this.currCursor);
     }
 
+    /**
+     * Populate a {@link BaseballCard} from the data in the current row of the
+     * given {@link Cursor}.
+     *
+     * @param cursor
+     *            The {@link Cursor} to obtain data from.
+     * @return A {@link BaseballCard} from the data in the current row of the
+     *         given {@link Cursor}.
+     */
     public BaseballCard getBaseballCardFromCursor(Cursor cursor) {
         String brand = cursor.getString(cursor.getColumnIndex(BaseballCardContract.BRAND_COL_NAME));
         int year = cursor.getInt(cursor.getColumnIndex(BaseballCardContract.YEAR_COL_NAME));
@@ -169,6 +277,15 @@ public class BaseballCardSQLHelper extends SQLiteOpenHelper {
         return new BaseballCard(brand, year, number, value, count, name, team, position);
     }
 
+    /**
+     * Populate a {@link List} of {@link BaseballCard}s from the data in the
+     * given {@link Cursor}.
+     *
+     * @param cursor
+     *            The {@link Cursor} to obtain data from.
+     * @return A {@link List} of {@link BaseballCard}s from the data in the
+     *         given {@link Cursor}.
+     */
     public List<BaseballCard> getAllBaseballCardsFromCursor(Cursor cursor) {
         Log.d(TAG, "getAllBaseballCardsFromCursor()");
         Log.d(TAG, "cursor=" + cursor);
@@ -183,6 +300,17 @@ public class BaseballCardSQLHelper extends SQLiteOpenHelper {
         return cards;
     }
 
+    /**
+     * Get the distinct values from the given column matching the given
+     * constraint.
+     *
+     * @param colName
+     *            The name of the column to query.
+     * @param constraint
+     *            An optional {@link String} to match against. May be
+     *            {@code null}.
+     * @return A {@link Cursor} containing the queried values.
+     */
     public Cursor getDistinctValues(String colName, String constraint) {
         String[] cols = {BaseballCardContract.ID_COL_NAME, colName};
         String filter = (constraint == null) ? null : colName + " LIKE ?";
