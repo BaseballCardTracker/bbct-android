639072ed272e501e9de093b097e4756b47e5eb09
codeguru <codeguru@users.sourceforge.net>
2014-01-30 15:49:40 -0700

    Javadoc: `BaseballCardList`

--- android/common/lib/src/bbct/android/common/activity/BaseballCardList.java
+++ android/common/lib/src/bbct/android/common/activity/BaseballCardList.java
@@ -166,7 +166,7 @@ public class BaseballCardList extends ListActivity {
      * Create the options menu. This is simply inflated from the
      * {@code option.xml} resource file.
      *
-     * @param The
+     * @param menu
      *            The options menu in which new menu items are placed.
      */
     @Override
