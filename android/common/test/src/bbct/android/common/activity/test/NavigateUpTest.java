60e7c519b960b767d381ec7349a7c046b12ee97f
codeguru <codeguru@users.sourceforge.net>
2014-02-01 17:52:57 -0700

    Issue #95: Stub out `NavigateUpTest`

---
+++ android/common/test/src/bbct/android/common/activity/test/NavigateUpTest.java
@@ -0,0 +1,22 @@
+package bbct.android.common.activity.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import bbct.android.common.activity.BaseballCardList;
+
+public class NavigateUpTest extends ActivityInstrumentationTestCase2<BaseballCardList> {
+
+    public NavigateUpTest() {
+        super(BaseballCardList.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+}
