41ac8c9c2f301a7f02085865aa42428fbd5f3ffa
codeguru <codeguru@users.sourceforge.net>
2014-02-01 18:13:32 -0700

    Issue #95: `testNavigateUpFromAbout()`

--- android/common/test/src/bbct/android/common/activity/test/NavigateUpTest.java
+++ android/common/test/src/bbct/android/common/activity/test/NavigateUpTest.java
@@ -1,7 +1,10 @@
 package bbct.android.common.activity.test;
 
 import android.test.ActivityInstrumentationTestCase2;
+import bbct.android.common.activity.About;
 import bbct.android.common.activity.BaseballCardList;
+import com.robotium.solo.Solo;
+import junit.framework.Assert;
 
 public class NavigateUpTest extends ActivityInstrumentationTestCase2<BaseballCardList> {
 
@@ -12,11 +15,17 @@ public class NavigateUpTest extends ActivityInstrumentationTestCase2<BaseballCar
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+
+        this.solo = new Solo(this.getInstrumentation(), this.getActivity());
     }
 
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
+    public void testNavigateUpFromAbout() {
+        this.solo.clickOnActionBarItem(bbct.android.common.R.id.about_menu);
+        Assert.assertTrue(this.solo.waitForActivity(About.class));
+        this.solo.clickOnActionBarHomeButton();
+        Assert.assertTrue(this.solo.waitForActivity(BaseballCardList.class));
     }
+
+    private Solo solo = null;
 
 }
