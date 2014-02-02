a0d6e31c830ab53d33feacdd09892bb2e362c7db
codeguru <codeguru@users.sourceforge.net>
2014-02-01 18:59:47 -0700

    Issue #95: `testNavigateUpFromDetails()`

--- android/common/test/src/bbct/android/common/activity/test/NavigateUpTest.java
+++ android/common/test/src/bbct/android/common/activity/test/NavigateUpTest.java
@@ -2,6 +2,7 @@ package bbct.android.common.activity.test;
 
 import android.test.ActivityInstrumentationTestCase2;
 import bbct.android.common.activity.About;
+import bbct.android.common.activity.BaseballCardDetails;
 import bbct.android.common.activity.BaseballCardList;
 import com.robotium.solo.Solo;
 import junit.framework.Assert;
@@ -19,12 +20,30 @@ public class NavigateUpTest extends ActivityInstrumentationTestCase2<BaseballCar
         this.solo = new Solo(this.getInstrumentation(), this.getActivity());
     }
 
-    public void testNavigateUpFromAbout() {
-        this.solo.clickOnActionBarItem(bbct.android.common.R.id.about_menu);
-        Assert.assertTrue(this.solo.waitForActivity(About.class));
-        this.solo.clickOnActionBarHomeButton();
-        Assert.assertTrue(this.solo.waitForActivity(BaseballCardList.class));
+    @Override
+    protected void tearDown() throws Exception {
+        this.solo.finishOpenedActivities();
+
+        super.tearDown();
     }
+
+    public void testNavigateUpFromAbout() {
+        this.solo.assertCurrentActivity("Expecting BaseballCardList", BaseballCardList.class);
+        this.solo.clickOnActionBarItem(bbct.android.common.R.id.about_menu);
+        Assert.assertTrue(this.solo.waitForActivity(About.class, TIMEOUT));
+        this.solo.clickOnActionBarHomeButton();
+        Assert.assertTrue(this.solo.waitForActivity(BaseballCardList.class, TIMEOUT));
+    }
+
+    public void testNavigateUpFromDetails() {
+        this.solo.assertCurrentActivity("Expecting BaseballCardList", BaseballCardList.class);
+        this.solo.clickOnActionBarItem(bbct.android.common.R.id.add_menu);
+        Assert.assertTrue(this.solo.waitForActivity(BaseballCardDetails.class, TIMEOUT));
+        this.solo.clickOnActionBarHomeButton();
+        Assert.assertTrue(this.solo.waitForActivity(BaseballCardList.class, TIMEOUT));
+    }
+
+    private static final int TIMEOUT = 500;
 
     private Solo solo = null;
 
