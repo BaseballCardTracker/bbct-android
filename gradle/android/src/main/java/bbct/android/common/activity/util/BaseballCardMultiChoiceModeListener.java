72a13571d98222128480bdca2499083decdef698
codeguru <codeguru@users.sourceforge.net>
2014-06-18 21:11:56 -0600

    Issue #178: Stub out `BaseballCardMultiChoiceModeListener`

---
+++ gradle/android/src/main/java/bbct/android/common/activity/util/BaseballCardMultiChoiceModeListener.java
@@ -0,0 +1,90 @@
+/*
+ * This file is part of BBCT for Android.
+ *
+ * Copyright 2012-14 codeguru <codeguru@users.sourceforge.net>
+ *
+ * BBCT for Android is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BBCT for Android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package bbct.android.common.activity.util;
+
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AbsListView;
+
+public class BaseballCardMultiChoiceModeListener implements AbsListView.MultiChoiceModeListener {
+
+    /**
+     * Called when an item is checked or unchecked during selection mode.
+     *
+     * @param mode     The {@link ActionMode} providing the selection mode
+     * @param position Adapter position of the item that was checked or unchecked
+     * @param id       Adapter ID of the item that was checked or unchecked
+     * @param checked  <code>true</code> if the item is now checked, <code>false</code>
+     */
+    @Override
+    public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+
+    }
+
+    /**
+     * Called when action mode is first created. The menu supplied will be used to
+     * generate action buttons for the action mode.
+     *
+     * @param mode ActionMode being created
+     * @param menu Menu used to populate action buttons
+     * @return true if the action mode should be created, false if entering this
+     * mode should be aborted.
+     */
+    @Override
+    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+        return false;
+    }
+
+    /**
+     * Called to refresh an action mode's action menu whenever it is invalidated.
+     *
+     * @param mode ActionMode being prepared
+     * @param menu Menu used to populate action buttons
+     * @return true if the menu or action mode was updated, false otherwise.
+     */
+    @Override
+    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+        return false;
+    }
+
+    /**
+     * Called to report a user click on an action button.
+     *
+     * @param mode The current ActionMode
+     * @param item The item that was clicked
+     * @return true if this callback handled the event, false if the standard MenuItem
+     * invocation should continue.
+     */
+    @Override
+    public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+        return false;
+    }
+
+    /**
+     * Called when an action mode is about to be exited and destroyed.
+     *
+     * @param mode The current ActionMode being destroyed
+     */
+    @Override
+    public void onDestroyActionMode(ActionMode mode) {
+
+    }
+
+}
