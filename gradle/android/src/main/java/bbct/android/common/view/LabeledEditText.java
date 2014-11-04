08c4edb2fce4670ab8e710e57e299876175b6ed2
Code Guru <codeguru42@gmail.com>
2014-11-03 21:49:07 -0700

    Add `LabeledEditText` custom compound view

---
+++ gradle/android/src/main/java/bbct/android/common/view/LabeledEditText.java
@@ -0,0 +1,75 @@
+/*
+ * This file is part of BBCT for Android.
+ *
+ * Copyright 2012-14 codeguru <codeguru@users.sourceforge.net>
+ *
+ * BBCT for Android is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BBCT for Android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package bbct.android.common.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import bbct.android.common.R;
+
+public class LabeledEditText extends LinearLayout {
+
+    public LabeledEditText(Context context) {
+        this(context, null);
+    }
+
+    public LabeledEditText(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public LabeledEditText(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        TextView label = new TextView(context);
+        EditText edit = new EditText(context);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.LabeledEditText,
+                0, 0);
+        int[] set = {android.R.attr.inputType, android.R.attr.singleLine};
+        TypedArray b = context.getTheme().obtainStyledAttributes(attrs, set, 0, 0);
+
+        try {
+            int labelWeight = a.getInt(R.styleable.LabeledEditText_labelWeight, 0);
+            ViewGroup.LayoutParams labelParams = new LinearLayout.LayoutParams(
+                    0, LayoutParams.WRAP_CONTENT, labelWeight);
+            label.setLayoutParams(labelParams);
+
+            int editWeight = a.getInt(R.styleable.LabeledEditText_editWeight, 0);
+            ViewGroup.LayoutParams editParams = new LinearLayout.LayoutParams(
+                    0, LayoutParams.WRAP_CONTENT, editWeight);
+            edit.setLayoutParams(editParams);
+
+            label.setText(a.getString(R.styleable.LabeledEditText_label));
+            label.setTextSize(a.getFloat(R.styleable.LabeledEditText_labelTextSize, 1.0f));
+            edit.setHint(a.getString(R.styleable.LabeledEditText_hint));
+            edit.setInputType(b.getInt(0, 0));
+            edit.setSingleLine(b.getBoolean(1, true));
+        } finally {
+            a.recycle();
+        }
+
+        this.addView(label);
+        this.addView(edit);
+    }
+
+}
