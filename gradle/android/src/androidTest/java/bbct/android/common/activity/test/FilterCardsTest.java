a2baae02378971cd6c33f6ee475268cd81933cb2
Code Apprentice <codeguru42@gmail.com>
2016-02-10 15:44:52 -0700

    Issue #331: Clean up imports in `FilterCardsTest`

--- gradle/android/src/androidTest/java/bbct/android/common/activity/test/FilterCardsTest.java
+++ gradle/android/src/androidTest/java/bbct/android/common/activity/test/FilterCardsTest.java
@@ -37,7 +37,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
@@ -48,10 +47,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
 
 /**
  * Tests for {@link FilterCards} activity class.
