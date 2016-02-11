e7c3879c571cb4a2795eeff5e62d22d71c79eb3b
Code Apprentice <codeguru42@gmail.com>
2016-02-11 09:08:33 -0700

    Abort if any step fails

--- gradle/release.sh
+++ gradle/release.sh
@@ -11,9 +11,9 @@ build_apk() {
     local version=$2
     local apk_prefix=${PREFIX}-${edition}
 
-    cd ${edition}/main
-    ant clean release
-    cp bin/${apk_prefix}-release.apk ${BIN_REL}/${edition}/${apk_prefix}-$version.apk
+    cd ${edition}/main &&
+    ant clean release &&
+    cp bin/${apk_prefix}-release.apk ${BIN_REL}/${edition}/${apk_prefix}-$version.apk &&
     cd -
 }
 
@@ -21,13 +21,13 @@ hash() {
     local edition=$1
     local prefix=${PREFIX}-${edition}
 
-    cd ${REL_DIR}/bin/android/${edition}
-    md5sum *.apk > ${prefix}.md5
-    echo Checking MD5...
-    md5sum -c ${prefix}.md5
-    sha1sum *.apk > ${prefix}.sha1
-    echo Checking SHA1...
-    sha1sum -c ${prefix}.sha1
+    cd ${REL_DIR}/bin/android/${edition} &&
+    md5sum *.apk > ${prefix}.md5 &&
+    echo Checking MD5... &&
+    md5sum -c ${prefix}.md5 &&
+    sha1sum *.apk > ${prefix}.sha1 &&
+    echo Checking SHA1... &&
+    sha1sum -c ${prefix}.sha1 &&
     cd -
 }
 
@@ -36,38 +36,38 @@ then {
     lite_version=$1
     premium_version=$2
 
-    echo Pull devel branch...
-    git stash save 'Stash before building release'
-    git checkout devel
-    git pull bbct devel
+    echo Pull devel branch... &&
+    git stash save 'Stash before building release' &&
+    git checkout devel &&
+    git pull bbct devel &&
 
-    echo Building APKs...
-    build_apk lite ${lite_version}
-    build_apk premium ${premium_version}
+    echo Building APKs... &&
+    build_apk lite ${lite_version} &&
+    build_apk premium ${premium_version} &&
 
-    echo Archiving source files...
-    git archive --format=zip --prefix=bbct/ -o ${SRC_REL}/bbct-lite-src.${lite_version}.zip master lite common
-    git archive --format=tar --prefix=bbct/ master lite common | gzip > ${SRC_REL}/bbct-lite-src.${lite_version}.tar.gz
+    echo Archiving source files... &&
+    git archive --format=zip --prefix=bbct/ -o ${SRC_REL}/bbct-lite-src.${lite_version}.zip master lite common &&
+    git archive --format=tar --prefix=bbct/ master lite common | gzip > ${SRC_REL}/bbct-lite-src.${lite_version}.tar.gz &&
 
-    echo Generating hashes...
-    hash lite
-    hash premium
+    echo Generating hashes... &&
+    hash lite &&
+    hash premium &&
 
-    cd ${REL_DIR}/src/android
-    md5sum *.tar.gz *.zip > $PREFIX-src.md5
-    echo Checking MD5...
-    md5sum -c $PREFIX-src.md5
-    sha1sum *.tar.gz *.zip > $PREFIX-src.sha1
-    echo Checking SHA1...
-    sha1sum -c $PREFIX-src.sha1
-    cd -
+    cd ${REL_DIR}/src/android &&
+    md5sum *.tar.gz *.zip > $PREFIX-src.md5 &&
+    echo Checking MD5... &&
+    md5sum -c $PREFIX-src.md5 &&
+    sha1sum *.tar.gz *.zip > $PREFIX-src.sha1 &&
+    echo Checking SHA1... &&
+    sha1sum -c $PREFIX-src.sha1 &&
+    cd - &&
 
-    echo Merge master...
-    git checkout master
-    git merge devel
+    echo Merge master... &&
+    git checkout master &&
+    git merge devel &&
 
-    echo Tag...
-    git tag l$lite_version
+    echo Tag... &&
+    git tag l$lite_version &&
     git tag p$premium_version
 }
 else {
