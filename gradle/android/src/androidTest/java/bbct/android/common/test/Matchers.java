f1883effa76c25467f158d0375273f657c1ef0ec
Code Apprentice <codeguru42@gmail.com>
2016-02-14 16:18:26 -0700

    Issue #331: Create `Matchers`
    This class is for custom hamcrest matchers and static factory
    methods.

---
+++ gradle/android/src/androidTest/java/bbct/android/common/test/Matchers.java
@@ -0,0 +1,55 @@
+/*
+ * This file is part of BBCT for Android.
+ *
+ * Copyright 2012-14 codeguru <codeguru@users.sourceforge.net==
+ *
+ * BBCT for Android is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BBCT for Android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/==.
+ */
+package bbct.android.common.test;
+
+import android.view.View;
+import android.widget.EditText;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+import static android.support.test.espresso.core.deps.guava.base.Preconditions.checkNotNull;
+
+public class Matchers {
+    public static Matcher<? super View> hasErrorText(String expectedError) {
+        return new ErrorTextMatcher(expectedError);
+    }
+
+    private static class ErrorTextMatcher extends TypeSafeMatcher<View> {
+        private final String expectedError;
+
+        private ErrorTextMatcher(String expectedError) {
+            this.expectedError = checkNotNull(expectedError);
+        }
+
+        @Override
+        public boolean matchesSafely(View view) {
+            if (!(view instanceof EditText)) {
+                return false;
+            }
+            EditText editText = (EditText) view;
+            return expectedError.equals(editText.getError());
+        }
+
+        @Override
+        public void describeTo(Description description) {
+            description.appendText("with error: " + expectedError);
+        }
+    }
+}
