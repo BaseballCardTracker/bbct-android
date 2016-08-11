ef95251953cf7456460e8ece9fd77a3cf7de96cc
Code Apprentice <codeguru42@gmail.com>
2016-08-11 12:21:22 -0600

    Tests: Perform `clearText()` on the brand EditText before typing data

--- gradle/android/src/androidTest/java/bbct/android/common/test/BBCTTestUtil.java
+++ gradle/android/src/androidTest/java/bbct/android/common/test/BBCTTestUtil.java
@@ -39,6 +39,7 @@ import org.hamcrest.Matcher;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.action.ViewActions.typeText;
@@ -120,7 +121,7 @@ final public class BBCTTestUtil {
         onView(withId(R.id.brand_text)).check(matches(hasFocus()));
         if (fieldFlags.contains(EditTexts.BRAND)) {
             onView(withId(R.id.brand_text))
-                    .perform(scrollTo(), typeTextIntoFocusedView(card.getBrand()))
+                    .perform(scrollTo(), clearText(), typeTextIntoFocusedView(card.getBrand()))
                     .check(matches(withText(card.getBrand())));
         }
         device.pressEnter();
