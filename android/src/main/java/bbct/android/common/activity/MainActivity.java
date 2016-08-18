61fbce943ae9edb8f609b9b62d0ee44fd6494968
Code Apprentice <codeguru42@gmail.com>
2016-08-17 19:43:55 -0600

    Change from `ActionBarActivity` to `AppCompatActivity`

--- android/src/main/java/bbct/android/common/activity/MainActivity.java
+++ android/src/main/java/bbct/android/common/activity/MainActivity.java
@@ -27,7 +27,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -39,7 +39,7 @@ import com.google.analytics.tracking.android.EasyTracker;
 import io.fabric.sdk.android.Fabric;
 import java.net.URISyntaxException;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
     public static final String PREFS = "bbct.prefs";
     public static final String SURVEY_TAKEN_PREF = "survey";
 
