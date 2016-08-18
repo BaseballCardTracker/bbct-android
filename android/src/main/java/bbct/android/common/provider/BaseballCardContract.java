4ad9951f87077697b86c59dd1d313b9ac76c3d74
Code Apprentice <codeguru42@gmail.com>
2016-08-17 19:23:35 -0600

    Remove unused import

--- android/src/main/java/bbct/android/common/provider/BaseballCardContract.java
+++ android/src/main/java/bbct/android/common/provider/BaseballCardContract.java
@@ -22,7 +22,6 @@ import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
-import android.util.Log;
 import bbct.android.common.data.BaseballCard;
 import java.util.ArrayList;
 import java.util.List;
