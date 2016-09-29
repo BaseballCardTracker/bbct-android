881f02fa258b8785b41d546123416b16a8057f02
Code Apprentice <codeguru42@gmail.com>
2016-09-28 19:11:44 -0600

    Implement `Survey2DialogTest`

--- android/src/androidTest/java/bbct/android/common/functional/test/Survey2DialogTest.java
+++ android/src/androidTest/java/bbct/android/common/functional/test/Survey2DialogTest.java
@@ -18,5 +18,100 @@
  */
 package bbct.android.common.functional.test;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import bbct.android.common.R;
+import bbct.android.common.SharedPreferenceKeys;
+import bbct.android.common.test.rule.Survey1SharedPreferencesTestRule;
+import bbct.android.common.test.rule.Survey2SharedPreferencesTestRule;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.startsWith;
+
+@RunWith(AndroidJUnit4.class)
 public class Survey2DialogTest {
+    @Rule
+    public Survey2SharedPreferencesTestRule prefsRule = new Survey2SharedPreferencesTestRule();
+
+    private SharedPreferences prefs;
+    private Context context;
+
+    @Before
+    public void setUp() throws UiObjectNotFoundException {
+        context = InstrumentationRegistry.getTargetContext();
+        startApp();
+        Intents.init();
+        prefs = prefsRule.getPrefs();
+    }
+
+    @After
+    public void tearDown() {
+        Intents.release();
+    }
+
+    @Test
+    public void testPreconditions() {
+        Assert.assertFalse(prefs.contains(SharedPreferenceKeys.SURVEY2_DATE));
+        Assert.assertTrue(prefs.contains(SharedPreferenceKeys.SURVEY1_DATE));
+    }
+
+    @Test
+    public void testTakeSurveyNow() throws UiObjectNotFoundException {
+        onView(withText(R.string.survey2))
+                .check(matches(isDisplayed()));
+        onView(withText(R.string.now))
+                .check(matches(isDisplayed()))
+                .perform(click());
+        Uri surveyUri = Uri.parse(context.getString(R.string.survey2_uri));
+        intended(allOf(hasAction(Intent.ACTION_VIEW), hasData(surveyUri)));
+        startApp();
+        Assert.assertTrue(prefs.contains(SharedPreferenceKeys.SURVEY2_DATE));
+        onView(withText(startsWith("BBCT")))
+                .check(matches(isDisplayed()));
+        onView(withText(R.string.survey1))
+                .check(doesNotExist());
+    }
+
+    @Test
+    public void testTakeSurveyLater() throws UiObjectNotFoundException {
+        onView(withText(R.string.survey1))
+                .check(matches(isDisplayed()));
+        onView(withText(R.string.later))
+                .check(matches(isDisplayed()))
+                .perform(click());
+        startApp();
+        Assert.assertFalse(prefs.contains(SharedPreferenceKeys.SURVEY_TAKEN_PREF));
+        onView(withText(R.string.survey1))
+                .check(matches(isDisplayed()));
+    }
+
+    private void startApp() {
+        String packageName = context.getPackageName();
+        Intent intent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
+    }
 }
