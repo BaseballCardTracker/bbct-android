49e2f4937eda9565cfc4002e2b8105d5e8d50bf0
Code Apprentice <codeguru42@gmail.com>
2017-06-23 11:35:47 -0600

    Write brandNext() test

--- android/src/androidTest/java/bbct/android/common/activity/test/DetailsNavigateNextTest.java
+++ android/src/androidTest/java/bbct/android/common/activity/test/DetailsNavigateNextTest.java
@@ -18,14 +18,38 @@
  */
 package bbct.android.common.activity.test;
 
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.KeyEvent;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import bbct.android.common.R;
+import bbct.android.common.activity.BaseballCardDetails;
+import bbct.android.common.activity.FragmentTestActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.pressKey;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasFocus;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
 @RunWith(AndroidJUnit4.class)
 public class DetailsNavigateNextTest {
+    @Rule
+    public ActivityTestRule<FragmentTestActivity> activityTestRule
+            = new ActivityTestRule<>(FragmentTestActivity.class);
+
     @Test
     public void brandNext() throws Exception {
+        FragmentTestActivity activity = activityTestRule.getActivity();
+        activity.replaceFragment(new BaseballCardDetails());
+        onView(withId(R.id.brand_text))
+                .check(matches(hasFocus()))
+                .perform(pressKey(KeyEvent.KEYCODE_ENTER));
+        onView(withId(R.id.year_text))
+                .check(matches(hasFocus()));
     }
 }
