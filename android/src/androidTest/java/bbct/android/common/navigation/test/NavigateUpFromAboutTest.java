2182a7b39a54bfc9c00de4df95f6cbb82d60991d
Jojo Narte <jojo@dnamicro.com>
2018-03-16 22:05:23 +0800

    NavigateUpFromAboutTest

---
+++ android/src/androidTest/java/bbct/android/common/navigation/test/NavigateUpFromAboutTest.java
@@ -0,0 +1,64 @@
+package bbct.android.common.navigation.test;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import bbct.android.common.R;
+import bbct.android.lite.provider.LiteActivity;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+/**
+ * Created by Joselito Narte Jr. on 16/03/2018.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class NavigateUpFromAboutTest {
+    @Rule
+    public ActivityTestRule<LiteActivity> activityActivityTestRule = new ActivityTestRule<LiteActivity>(LiteActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+        activityActivityTestRule.getActivity()
+                .getSupportFragmentManager().beginTransaction();
+    }
+
+    @Test
+    public void testNavigateToAboutFragment() {
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+
+        String aboutTitle = getInstrumentation().getTargetContext().getString(R.string.about_title);
+        onView(allOf(withText(aboutTitle), isDisplayed())).perform(click());
+
+        String title = getInstrumentation().getTargetContext().getString(R.string.bbct_title, aboutTitle);
+        onView(withText(title)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testNavigateWithHomeIcon() {
+        String initialTitle = (String) activityActivityTestRule.getActivity().getTitle();
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+
+        String aboutTitle = getInstrumentation().getTargetContext().getString(R.string.about_title);
+
+        onView(allOf(withText(aboutTitle), isDisplayed())).perform(click());
+        String title = getInstrumentation().getTargetContext().getString(R.string.bbct_title, aboutTitle);
+        onView(withText(title)).check(matches(isDisplayed()));
+
+        onView(allOf(withContentDescription(R.string.abc_action_bar_up_description), isDisplayed())).perform(click());
+        onView(withText(initialTitle)).check(matches(isDisplayed()));
+    }
+}
---
+++ android/src/androidTest/java/bbct/android/common/navigation/test/NavigateUpFromBaseballCardListTest.java
@@ -0,0 +1,41 @@
+package bbct.android.common.navigation.test;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import bbct.android.common.test.rule.DataTestRule;
+import bbct.android.lite.provider.LiteActivity;
+import bbct.data.BaseballCard;
+
+/**
+ * Created by Joselito Narte Jr. on 16/03/2018.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class NavigateUpFromBaseballCardListTest {
+
+    @Rule
+    public DataTestRule dataTestRule = new DataTestRule();
+
+    @Rule
+    public ActivityTestRule<LiteActivity> activityActivityTestRule = new ActivityTestRule<LiteActivity>(LiteActivity.class);
+
+    private List<BaseballCard> allCards;
+
+    @Before
+    public void setUp() throws Exception {
+        activityActivityTestRule.getActivity()
+                .getSupportFragmentManager().beginTransaction();
+
+        allCards = dataTestRule.getAllCards();
+    }
+
+
+}
