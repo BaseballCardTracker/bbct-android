8b4da473345c040d856c013d4e3a10209516f26c
Jojo Narte <jojo@dnamicro.com>
2018-03-16 22:05:23 +0800

    NavigateUpFromAboutTest

---
+++ android/src/androidTest/java/bbct/android/common/navigation/test/NavigateUpFromBaseballCardDetailsTest.java
@@ -0,0 +1,47 @@
+package bbct.android.common.navigation.test;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+/**
+ * Created by Joselito Narte Jr. on 16/03/2018.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class NavigateUpFromBaseballCardDetailsTest {
+
+//    @Rule
+//    public ActivityTestRule<LiteActivity> activityActivityTestRule = new ActivityTestRule<LiteActivity>(LiteActivity.class);
+//
+//    @Before
+//    public void setUp() throws Exception {
+//        activityActivityTestRule.getActivity()
+//                .getSupportFragmentManager().beginTransaction();
+//    }
+//
+//    @Test
+//    public void testNavigateToAboutFragment() {
+//        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+//        onView(allOf(withText("About"), isDisplayed())).perform(click());
+//
+//        String aboutTitle = getInstrumentation().getTargetContext().getString(R.string.about_title);
+//        String title = getInstrumentation().getTargetContext().getString(R.string.bbct_title, aboutTitle);
+//        onView(withText(title)).check(matches(isDisplayed()));
+//    }
+//
+//    @Test
+//    public void testNavigateWithHomeIcon() {
+//        String initialTitle = (String) activityActivityTestRule.getActivity().getTitle();
+//        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+//
+//        String aboutTitle = getInstrumentation().getTargetContext().getString(R.string.about_title);
+//
+//        onView(allOf(withText(aboutTitle), isDisplayed())).perform(click());
+//        String title = getInstrumentation().getTargetContext().getString(R.string.bbct_title, aboutTitle);
+//        onView(withText(title)).check(matches(isDisplayed()));
+//
+//        onView(allOf(withContentDescription(R.string.abc_action_bar_up_description), isDisplayed())).perform(click());
+//        onView(withText(initialTitle)).check(matches(isDisplayed()));
+//    }
+}
