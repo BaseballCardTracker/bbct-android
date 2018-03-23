038ba03ad6e1c415e375ec14a3d51e8cd1106ca8
Jojo Narte <jojo@dnamicro.com>
2018-03-23 08:11:52 +0800

    license

--- android/src/androidTest/java/bbct/android/common/navigation/test/NavigateUpFromAboutTest.java
+++ android/src/androidTest/java/bbct/android/common/navigation/test/NavigateUpFromAboutTest.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of BBCT for Android.
+ *
+ * Copyright 2018 codeguru <codeguru@users.sourceforge.net>
+ *
+ * BBCT for Android is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BBCT for Android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package bbct.android.common.navigation.test;
 
 import android.support.test.rule.ActivityTestRule;
@@ -21,9 +39,6 @@ import static android.support.test.espresso.matcher.ViewMatchers.withContentDesc
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
-/**
- * Created by Joselito Narte Jr. on 16/03/2018.
- */
 
 @RunWith(AndroidJUnit4.class)
 public class NavigateUpFromAboutTest {
