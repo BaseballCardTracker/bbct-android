32c12ebcab02a0897d708a92fca11d86a62a7c85
Jojo Narte <Jojo@dnamicro.com>
2018-04-01 21:03:51 +0800

    NavigateBackPressFromBaseballCardListTest

--- android/src/androidTest/java/bbct/android/common/navigation/test/NavigateBackPressFromBaseballCardListTest.java
+++ android/src/androidTest/java/bbct/android/common/navigation/test/NavigateBackPressFromBaseballCardListTest.java
@@ -1,10 +1,41 @@
 package bbct.android.common.navigation.test;
 
+import android.support.test.espresso.Espresso;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+
+import bbct.android.common.R;
+import bbct.android.lite.provider.LiteActivity;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class NavigateBackPressFromBaseballCardListTest {
+    @Rule
+    public ActivityTestRule<LiteActivity> activityActivityTestRule = new ActivityTestRule<LiteActivity>(LiteActivity.class);
 
+
+    @Before
+    public void setUp() throws Exception {
+        activityActivityTestRule.getActivity()
+                .getSupportFragmentManager().beginTransaction();
+    }
+
+    @Test
+    public void testBackPress() {
+        String expectedTitle = getInstrumentation().getTargetContext().getString(R.string.app_name);
+        Espresso.closeSoftKeyboard();
+        Espresso.pressBack();
+        onView(withText(expectedTitle)).check(matches(isDisplayed()));
+    }
 }
