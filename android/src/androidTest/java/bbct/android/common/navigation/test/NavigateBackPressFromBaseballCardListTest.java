7e985e6a01016a08e56ad2e6577d83b32fae85c7
Jojo Narte <Jojo@dnamicro.com>
2018-04-01 19:56:36 +0800

    prepare test classes

---
+++ android/src/androidTest/java/bbct/android/common/navigation/test/NavigateBackPressFromBaseballCardListTest.java
@@ -0,0 +1,10 @@
+package bbct.android.common.navigation.test;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class NavigateBackPressFromBaseballCardListTest {
+
+}
