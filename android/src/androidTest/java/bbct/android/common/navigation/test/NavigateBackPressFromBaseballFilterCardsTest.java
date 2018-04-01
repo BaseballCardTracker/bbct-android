8f4c9d4336a82023b384f0b6f635e28696ef1700
Jojo Narte <Jojo@dnamicro.com>
2018-04-01 20:51:15 +0800

    navigate using backpress from filtercards

--- android/src/androidTest/java/bbct/android/common/navigation/test/NavigateBackPressFromBaseballFilterCardsTest.java
+++ android/src/androidTest/java/bbct/android/common/navigation/test/NavigateBackPressFromBaseballFilterCardsTest.java
@@ -1,4 +1,44 @@
 package bbct.android.common.navigation.test;
 
+import android.support.test.espresso.Espresso;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import bbct.android.common.R;
+import bbct.android.lite.provider.LiteActivity;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@RunWith(AndroidJUnit4.class)
 public class NavigateBackPressFromBaseballFilterCardsTest {
+    @Rule
+    public ActivityTestRule<LiteActivity> activityActivityTestRule = new ActivityTestRule<LiteActivity>(LiteActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+        activityActivityTestRule.getActivity()
+                .getSupportFragmentManager().beginTransaction();
+    }
+
+    @Test
+    public void testNavigateUp() {
+
+        String expectedTitle = getInstrumentation().getTargetContext().getString(R.string.app_name);
+        Espresso.pressBack();
+        onView(allOf(withContentDescription(R.string.filter_cards_title), isDisplayed())).perform(click());
+        Espresso.pressBack();
+        onView(allOf(withText(expectedTitle), isDisplayed()));
+
+    }
 }
