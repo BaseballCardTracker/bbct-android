238de3161135d05755a663dc51cafb3e74befd9e
Jojo Narte <Jojo@dnamicro.com>
2018-04-01 19:56:51 +0800

    Navigatebackpress from about test

--- android/src/androidTest/java/bbct/android/common/navigation/test/NavigateBackPressFromBaseballCardDetailsTest.java
+++ android/src/androidTest/java/bbct/android/common/navigation/test/NavigateBackPressFromBaseballCardDetailsTest.java
@@ -1,4 +1,9 @@
 package bbct.android.common.navigation.test;
 
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
 public class NavigateBackPressFromBaseballCardDetailsTest {
 }
