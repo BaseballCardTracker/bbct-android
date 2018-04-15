3a061669b0f8ae288a4e8857bf33bd83ca2032e1
Jojo Narte <Jojo@dnamicro.com>
2018-04-15 22:33:11 +0800

    set up tests

---
+++ android/src/androidTest/java/bbct/android/common/navigation/test/NavigateBackPressFromAboutTest.java
@@ -0,0 +1,80 @@
+/*
+ * This file is part of BBCT for Android.
+ *
+ * Copyright 2018 codeguru <codeguru@users.sourceforge.net>
+ *
+ * BBCT for Android is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BBCT for Android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package bbct.android.common.navigation.test;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import bbct.android.common.R;
+import bbct.android.lite.provider.LiteActivity;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@RunWith(AndroidJUnit4.class)
+public class NavigateBackPressFromAboutTest {
+    @Rule
+    public ActivityTestRule<LiteActivity> activityActivityTestRule = new ActivityTestRule<LiteActivity>(LiteActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+        activityActivityTestRule.getActivity()
+                .getSupportFragmentManager().beginTransaction();
+    }
+
+    @Test
+    public void testNavigateToAboutFragment() {
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+
+        String aboutTitle = getInstrumentation().getTargetContext().getString(R.string.about_title);
+        Espresso.pressBack();
+
+        String expectedTitle = getInstrumentation().getTargetContext().getString(R.string.bbct_title, aboutTitle);
+        onView(withText(expectedTitle)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testNavigateBack() {
+        String initialTitle = (String) activityActivityTestRule.getActivity().getTitle();
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+
+        String aboutTitle = getInstrumentation().getTargetContext().getString(R.string.about_title);
+
+        onView(allOf(withText(aboutTitle), isDisplayed())).perform(click());
+        String expectedTitle = getInstrumentation().getTargetContext().getString(R.string.bbct_title, aboutTitle);
+        onView(withText(expectedTitle)).check(matches(isDisplayed()));
+
+        Espresso.closeSoftKeyboard();
+        Espresso.pressBack();
+        onView(withText(initialTitle)).check(matches(isDisplayed()));
+    }
+}
