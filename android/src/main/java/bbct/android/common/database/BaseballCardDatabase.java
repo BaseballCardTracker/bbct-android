a54dec68c0aafaede465e5fa1c8fd7e870048077
Code Apprentice <codeguru42@gmail.com>
2018-07-26 19:59:27 -0600

    Implement BaseballCardDatabase as a singleton

--- android/src/main/java/bbct/android/common/database/BaseballCardDatabase.java
+++ android/src/main/java/bbct/android/common/database/BaseballCardDatabase.java
@@ -1,11 +1,28 @@
 package bbct.android.common.database;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
 import bbct.android.common.provider.BaseballCardSQLHelper;
 
 @Database(version = BaseballCardSQLHelper.ROOM_SCHEMA, entities = {BaseballCard.class})
 public abstract class BaseballCardDatabase extends RoomDatabase {
+    private static BaseballCardDatabase instance = null;
+    private static final String DATABASE_NAME = "bbct.db";
+
+    public static BaseballCardDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(
+                    context.getApplicationContext(),
+                    BaseballCardDatabase.class,
+                    DATABASE_NAME)
+                    .build();
+        }
+
+        return instance;
+    }
+
     abstract public BaseballCardDao getBaseballCardDao();
 }
