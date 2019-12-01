c217e4cb8b7c2f83d08f2e3d749076266ae7ded4
Code-Apprentice <codeguru42@gmail.com>
2019-12-01 15:03:48 -0700

    Stub navigation tests

--- android/src/androidTest/java/bbct/android/common/navigation/test/NavigationTest.java
+++ android/src/androidTest/java/bbct/android/common/navigation/test/NavigationTest.java
@@ -1,4 +1,36 @@
 package bbct.android.common.navigation.test;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class NavigationTest {
+    @Test
+    void appStartsOnListView() {
+        Assert.fail("NOT IMPLEMENTED");
+    }
+
+    @Test
+    void clickOnAddButtonNavigatesToDetails() {
+        Assert.fail("NOT IMPLEMENTED");
+    }
+
+    @Test
+    void clickOnListItemNavigatesToDetails() {
+        Assert.fail("NOT IMPLEMENTED");
+    }
+
+    @Test
+    void clickOnSearchMenuNavigatesToFilter() {
+        Assert.fail("NOT IMPLEMENTED");
+    }
+
+    @Test
+    void clickOnApplyNavigatesBackToList() {
+        Assert.fail("NOT IMPLEMENTED");
+    }
+
+    @Test
+    void clickOnAboutMenuNavigatesToAbout() {
+        Assert.fail("NOT IMPLEMENTED");
+    }
 }
