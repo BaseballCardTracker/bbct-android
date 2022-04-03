15040d9f98e2d854290c0d18a48d269cf0a587ee
Code-Apprentice <codeguru42@gmail.com>
2022-04-02 21:38:36 -0600

    Use IP address that the emulator can access

--- android/src/main/java/bbct/android/common/api/RetrofitClient.java
+++ android/src/main/java/bbct/android/common/api/RetrofitClient.java
@@ -3,7 +3,7 @@ package bbct.android.common.api;
 import retrofit2.Retrofit;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://localhost:8000/api/";
+    private static final String BASE_URL = "http://10.0.2.2:8000/api/";
 
     private static Retrofit retrofit;
 
