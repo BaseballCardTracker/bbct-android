88db30384acda63da69bb1816965dd79da8152f2
Code-Apprentice <codeguru42@gmail.com>
2022-04-02 21:45:19 -0600

    Log OkHttp in debug build

--- android/src/main/java/bbct/android/common/api/RetrofitClient.java
+++ android/src/main/java/bbct/android/common/api/RetrofitClient.java
@@ -1,5 +1,8 @@
 package bbct.android.common.api;
 
+import bbct.android.common.BuildConfig;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -10,8 +13,16 @@ public class RetrofitClient {
 
     public static Retrofit getInstance() {
         if (retrofit == null) {
-            retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
+            Retrofit.Builder builder = new Retrofit.Builder();
+
+            if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+                interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+                OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+                builder.client(client);
+            }
+
+            retrofit = builder.baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         }
