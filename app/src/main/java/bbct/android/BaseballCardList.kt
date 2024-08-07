efba08dc85cb40c60f4f26399fde33da8f48a7d7
Code-Apprentice <codeguru42@gmail.com>
2024-08-06 23:42:31 -0600

    Use padding

--- app/src/main/java/bbct/android/BaseballCardList.kt
+++ app/src/main/java/bbct/android/BaseballCardList.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 val cards = listOf(
     BaseballCard(
@@ -45,8 +46,8 @@ val cards = listOf(
 )
 
 @Composable
-fun BaseballCardList() {
-    LazyColumn {
+fun BaseballCardList(modifier: Modifier) {
+    LazyColumn(modifier = modifier) {
         items(cards) { card ->
             BaseballCardRow(card)
         }
--- app/src/main/java/bbct/android/MainActivity.kt
+++ app/src/main/java/bbct/android/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import bbct.android.ui.theme.BbctTheme
@@ -16,7 +17,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             BbctTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    BaseballCardList()
+                    BaseballCardList(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
