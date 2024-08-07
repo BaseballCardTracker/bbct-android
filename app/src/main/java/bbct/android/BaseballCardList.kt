4ae19388961159f3436dc31146d264373cb0a248
Code-Apprentice <codeguru42@gmail.com>
2024-08-06 22:34:14 -0600

    Mock card data

---
+++ app/src/main/java/bbct/android/BaseballCardList.kt
@@ -0,0 +1,40 @@
+package bbct.android
+
+val cards = listOf(
+    BaseballCard(
+        autographed = false,
+        condition = "Excellent",
+        brand = "Topps",
+        year = 1991,
+        number = "278",
+        value = 500,
+        quantity = 1,
+        playerName = "Alex Fernandez",
+        team = "White Sox",
+        position = "Pitcher"
+    ),
+    BaseballCard(
+        autographed = true,
+        condition = "Mint",
+        brand = "Topps",
+        year = 1974,
+        number = "175",
+        value = 1000,
+        quantity = 1,
+        playerName = "Bob Stanley",
+        team = "Red Sox",
+        position = "Pitcher"
+    ),
+    BaseballCard(
+        autographed = false,
+        condition = "Very Good",
+        brand = "Topps",
+        year = 1985,
+        number = "201",
+        value = 200,
+        quantity = 1,
+        playerName = "Vince Coleman",
+        team = "Cardinals",
+        position = "Left Field",
+    ),
+)
