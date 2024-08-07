a758b918fea97d8c87c14b4c594d05a67734313c
Code-Apprentice <codeguru42@gmail.com>
2024-08-07 01:06:26 -0600

    Add a top bar

--- app/src/main/java/bbct/android/MainActivity.kt
+++ app/src/main/java/bbct/android/MainActivity.kt
@@ -6,7 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import bbct.android.ui.theme.BbctTheme
 
@@ -16,10 +20,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BbctTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(topBar = { TopBar() }, modifier = Modifier.fillMaxSize()) { innerPadding ->
                     BaseballCardList(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
+    }
+
+    @Composable
+    @OptIn(ExperimentalMaterial3Api::class)
+    private fun TopBar() {
+        TopAppBar(title = { Text("BBCT") })
     }
 }
