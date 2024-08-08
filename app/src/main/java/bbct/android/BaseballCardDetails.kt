014b2d3010d4c168316a983adee0d087482b2c03
Code-Apprentice <codeguru42@gmail.com>
2024-08-07 21:21:38 -0600

    BaseballCardDetails stub

---
+++ app/src/main/java/bbct/android/BaseballCardDetails.kt
@@ -0,0 +1,9 @@
+package bbct.android
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BaseballCardDetails(card: BaseballCard) {
+    Text("Baseball Card Details")
+}
