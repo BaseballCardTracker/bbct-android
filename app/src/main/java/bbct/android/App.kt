38e97e67526e349257c9508322098116b6b38867
Code-Apprentice <codeguru42@gmail.com>
2024-08-07 20:55:42 -0600

    Add filter menu button

--- app/src/main/java/bbct/android/App.kt
+++ app/src/main/java/bbct/android/App.kt
@@ -4,9 +4,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -26,7 +28,14 @@ fun App() {
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
 private fun TopBar() {
-    TopAppBar(title = { Text("BBCT") })
+    TopAppBar(title = { Text("BBCT") }, actions = { MainMenu() })
+}
+
+@Composable
+fun MainMenu() {
+    IconButton(onClick = { /*TODO*/ }) {
+        Icon(Icons.Default.Search, contentDescription = "Filter Cards")
+    }
 }
 
 @Composable
