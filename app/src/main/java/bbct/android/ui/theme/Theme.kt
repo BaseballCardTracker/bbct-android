668ec74a33d0bf026745aa30192c5443364aa44f
Code-Apprentice <codeguru42@gmail.com>
2024-08-07 20:26:28 -0600

    Copy colors from XML

--- app/src/main/java/bbct/android/ui/theme/Theme.kt
+++ app/src/main/java/bbct/android/ui/theme/Theme.kt
@@ -12,15 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Blue700,
+    secondary = GreenA200,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Blue500,
+    secondary = GreenA200,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
