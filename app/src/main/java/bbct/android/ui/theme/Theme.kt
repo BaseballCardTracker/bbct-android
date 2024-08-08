f367bc5c3ab7e4d94d6d6840332d82615401a44c
Code-Apprentice <codeguru42@gmail.com>
2024-08-08 13:15:44 -0600

    Remove commented code

--- app/src/main/java/bbct/android/ui/theme/Theme.kt
+++ app/src/main/java/bbct/android/ui/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.graphics.Color
 private val DarkColorScheme = darkColorScheme(
     primary = Blue700,
     secondary = GreenA200,
+
     surface = Blue700,
 )
 
@@ -21,16 +22,6 @@ private val LightColorScheme = lightColorScheme(
     onSurface = Color.White,
 
     primaryContainer = GreenA200,
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
