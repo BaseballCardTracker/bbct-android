00e04340cc36ad0bfad778ef4549f20acd54f60c
Code-Apprentice <codeguru42@gmail.com>
2024-08-09 12:48:44 -0600

    Select composable for Condition and Position fields

--- app/src/main/java/bbct/android/BaseballCardDetailsScreen.kt
+++ app/src/main/java/bbct/android/BaseballCardDetailsScreen.kt
@@ -1,15 +1,29 @@
 package bbct.android
 
+import android.content.Context
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 
 @Composable
-fun BaseballCardDetailsScreen() {
+fun BaseballCardDetailsScreen(context: Context = LocalContext.current) {
+    val resources = context.resources
+    val conditions = resources.getStringArray(R.array.condition)
+    val positions = resources.getStringArray(R.array.positions)
+
     Column {
         Row {
             Checkbox(checked = false, onCheckedChange = { /* TODO */ })
@@ -17,6 +31,10 @@ fun BaseballCardDetailsScreen() {
         }
         Row {
             Text(text = stringResource(id = R.string.condition_label))
+            Select(
+                options = conditions,
+                selected = conditions[0],
+                onSelectedChange = { /* TODO */ })
         }
         Row {
             Text(text = stringResource(id = R.string.brand_label))
@@ -48,6 +66,40 @@ fun BaseballCardDetailsScreen() {
         }
         Row {
             Text(text = stringResource(id = R.string.player_position_label))
+            Select(
+                options = positions,
+                selected = positions[0],
+                onSelectedChange = { /* TODO */ })
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Select(options: Array<String>, selected: String, onSelectedChange: (String) -> Unit) {
+    var expanded by remember { mutableStateOf(false) }
+
+    ExposedDropdownMenuBox(expanded = expanded, onExpandedChange = { expanded = !expanded }) {
+        TextField(
+            readOnly = true,
+            value = selected,
+            onValueChange = { /* TODO */ },
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(
+                    expanded = expanded
+                )
+            },
+            colors = ExposedDropdownMenuDefaults.textFieldColors()
+        )
+        ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+            options.forEach { option ->
+                DropdownMenuItem(
+                    text = { Text(text = option) },
+                    onClick = {
+                        onSelectedChange(option)
+                        expanded = false
+                    })
+            }
         }
     }
 }
