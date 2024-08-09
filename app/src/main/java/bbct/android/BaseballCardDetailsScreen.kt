04f3ac507a0a91c0dbfd6794d9107f056b12b1ba
Code-Apprentice <codeguru42@gmail.com>
2024-08-09 09:19:12 -0600

    Add controls to BaseballCardDetailsScreen

--- app/src/main/java/bbct/android/BaseballCardDetailsScreen.kt
+++ app/src/main/java/bbct/android/BaseballCardDetailsScreen.kt
@@ -1,9 +1,53 @@
 package bbct.android
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 
 @Composable
 fun BaseballCardDetailsScreen() {
-    Text("Baseball Card Details")
+    Column {
+        Row {
+            Checkbox(checked = false, onCheckedChange = { /* TODO */ })
+            Text(text = stringResource(id = R.string.autograph_label))
+        }
+        Row {
+            Text(text = stringResource(id = R.string.condition_label))
+        }
+        Row {
+            Text(text = stringResource(id = R.string.brand_label))
+            TextField(value = "Topps", onValueChange = { /* TODO */ })
+        }
+        Row {
+            Text(text = stringResource(id = R.string.year_label))
+            TextField(value = "1991", onValueChange = { /* TODO */ })
+        }
+        Row {
+            Text(text = stringResource(id = R.string.number_label))
+            TextField(value = "278", onValueChange = { /* TODO */ })
+        }
+        Row {
+            Text(text = stringResource(id = R.string.value_label))
+            TextField(value = "500", onValueChange = { /* TODO */ })
+        }
+        Row {
+            Text(text = stringResource(id = R.string.count_label))
+            TextField(value = "1", onValueChange = { /* TODO */ })
+        }
+        Row {
+            Text(text = stringResource(id = R.string.player_name_label))
+            TextField(value = "Alex Fernandez", onValueChange = { /* TODO */ })
+        }
+        Row {
+            Text(text = stringResource(id = R.string.team_label))
+            TextField(value = "White Sox", onValueChange = { /* TODO */ })
+        }
+        Row {
+            Text(text = stringResource(id = R.string.player_position_label))
+        }
+    }
 }
