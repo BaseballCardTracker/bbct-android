8327e8d0f3f2b5b59073cee49e5911f569e68f4b
Code-Apprentice <codeguru42@gmail.com>
2024-08-10 07:11:38 -0600

    BaseballCardFilterScreen implementation

--- app/src/main/java/bbct/android/ui/BaseballCardFilterScreen.kt
+++ app/src/main/java/bbct/android/ui/BaseballCardFilterScreen.kt
@@ -1,10 +1,78 @@
 package bbct.android.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+import bbct.android.R
 
 @Composable
 fun BaseballCardFilterScreen(navController: NavController) {
-    Text("BaseballCardFilterScreen")
+    Scaffold(
+        topBar = { TopBar(navController) },
+        floatingActionButton = { ApplyFilterButton(navController) },
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+        BaseballCardFilter(modifier = Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun BaseballCardFilter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Row {
+            Checkbox(checked = false, onCheckedChange = { /*TODO*/ })
+            TextField(
+                label = { Text(text = stringResource(id = R.string.brand)) },
+                value = "",
+                onValueChange = { /*TODO*/ })
+        }
+        Row {
+            Checkbox(checked = false, onCheckedChange = { /*TODO*/ })
+            TextField(
+                label = { Text(text = stringResource(id = R.string.year)) },
+                value = "",
+                onValueChange = { /*TODO*/ })
+        }
+        Row {
+            Checkbox(checked = false, onCheckedChange = { /*TODO*/ })
+            TextField(
+                label = { Text(text = stringResource(id = R.string.number)) },
+                value = "",
+                onValueChange = { /*TODO*/ })
+        }
+        Row {
+            Checkbox(checked = false, onCheckedChange = { /*TODO*/ })
+            TextField(
+                label = { Text(text = stringResource(id = R.string.player_name)) },
+                value = "",
+                onValueChange = { /*TODO*/ })
+        }
+        Row {
+            Checkbox(checked = false, onCheckedChange = { /*TODO*/ })
+            TextField(
+                label = { Text(text = stringResource(id = R.string.team)) },
+                value = "",
+                onValueChange = { /*TODO*/ })
+        }
+    }
+}
+
+@Composable
+fun ApplyFilterButton(navController: NavController) {
+    FloatingActionButton(onClick = { /* TODO */ }) {
+        Icon(Icons.Default.Check, contentDescription = stringResource(id = R.string.filter_menu))
+    }
 }
