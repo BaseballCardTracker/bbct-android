ffb8a53ce4fc9a7be31e4a218c2bd904c7cc99d2
Code-Apprentice <codeguru42@gmail.com>
2024-08-11 09:51:46 -0600

    Create database instance

--- app/src/main/java/bbct/android/MainActivity.kt
+++ app/src/main/java/bbct/android/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import bbct.android.data.BaseballCardDatabase
 import bbct.android.ui.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val db = BaseballCardDatabase.getInstance(this)
         setContent {
             App()
         }
