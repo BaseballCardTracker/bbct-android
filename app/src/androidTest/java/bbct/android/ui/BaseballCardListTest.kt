378fb5f2719da5e277e75bc886b4cc8c4ad48e2e
Code-Apprentice <codeguru42@gmail.com>
2024-08-18 20:11:37 -0600

    Refactor data to file

--- app/src/androidTest/java/bbct/android/ui/BaseballCardListTest.kt
+++ app/src/androidTest/java/bbct/android/ui/BaseballCardListTest.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithText
 import androidx.navigation.compose.rememberNavController
-import bbct.android.data.BaseballCard
+import bbct.android.data.cards
 import org.junit.Rule
 import org.junit.Test
 
@@ -15,48 +15,6 @@ class BaseballCardListTest {
 
     @Test
     fun testBaseballCardList() {
-        val cards = listOf(
-            BaseballCard(
-                _id = 1,
-                autographed = false,
-                condition = "Excellent",
-                brand = "Topps",
-                year = 1991,
-                number = "278",
-                value = 500,
-                quantity = 1,
-                playerName = "Alex Fernandez",
-                team = "White Sox",
-                position = "Pitcher"
-            ),
-            BaseballCard(
-                _id = 2,
-                autographed = true,
-                condition = "Mint",
-                brand = "Topps",
-                year = 1974,
-                number = "175",
-                value = 1000,
-                quantity = 1,
-                playerName = "Bob Stanley",
-                team = "Red Sox",
-                position = "Pitcher"
-            ),
-            BaseballCard(
-                _id = 3,
-                autographed = false,
-                condition = "Very Good",
-                brand = "Topps",
-                year = 1985,
-                number = "201",
-                value = 200,
-                quantity = 1,
-                playerName = "Vince Coleman",
-                team = "Cardinals",
-                position = "Left Field",
-            ),
-        )
-
         composeTestRule.setContent {
             val navController = rememberNavController()
             BaseballCardList(navController, cards)
