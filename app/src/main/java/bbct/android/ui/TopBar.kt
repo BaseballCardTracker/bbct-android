eb4e04db093cb0113b36488b2baf5476841874ec
Code-Apprentice <codeguru42@gmail.com>
2024-08-20 00:20:49 -0600

    Overflow menu

--- app/src/main/java/bbct/android/ui/TopBar.kt
+++ app/src/main/java/bbct/android/ui/TopBar.kt
@@ -3,13 +3,20 @@ package bbct.android.ui
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import bbct.android.R
@@ -30,8 +37,21 @@ fun TopBar(
 
 @Composable
 fun MainMenu(navController: NavController) {
+    var showMenu by remember { mutableStateOf(false) }
+
     IconButton(onClick = { navController.navigate(BaseballCardFilterDestination.route) }) {
         Icon(Icons.Default.Search, contentDescription = stringResource(id = R.string.filter_menu))
+    }
+    IconButton(onClick = { showMenu = !showMenu }) {
+        Icon(Icons.Default.MoreVert, contentDescription = stringResource(id = R.string.more))
+    }
+    DropdownMenu(
+        expanded = showMenu,
+        onDismissRequest = { showMenu = false }
+    ) {
+        DropdownMenuItem(
+            text = { Text(text = stringResource(id = R.string.about_menu)) },
+            onClick = { /*TODO*/ })
     }
 }
 
