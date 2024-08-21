28e566ac074f737cd7408ed1d17f28682b24a3e7
Code-Apprentice <codeguru42@gmail.com>
2024-08-21 11:25:38 -0600

    Fix app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "bbct.android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 24
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
