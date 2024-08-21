7abf1eb09436ef46b5afb671a93b6347395080da
Code-Apprentice <codeguru42@gmail.com>
2024-08-21 10:46:07 -0600

    Disable crashlytics in debug build

--- app/src/main/java/bbct/android/MainActivity.kt
+++ app/src/main/java/bbct/android/MainActivity.kt
@@ -6,9 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import bbct.android.data.BaseballCardDatabase
 import bbct.android.ui.App
+import com.google.firebase.Firebase
+import com.google.firebase.crashlytics.crashlytics
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Firebase.crashlytics.setCrashlyticsCollectionEnabled(!BuildConfig.DEBUG)
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         val db = BaseballCardDatabase.getInstance(this, BaseballCardDatabase.DATABASE_NAME)
