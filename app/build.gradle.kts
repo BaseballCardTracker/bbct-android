3ae32792fe87aa03da085fb975fc545acbfbe1bd
Code-Apprentice <codeguru42@gmail.com>
2024-08-22 01:18:16 -0600

    Update versionCode and versionName to match build on Google Play...for real this time

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "bbct.android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 24
-        versionName = "1.1.0"
+        versionCode = 25
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
