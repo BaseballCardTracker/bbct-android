ac46511eb2de8df584f6a20a91281711941dd386
Code-Apprentice <codeguru42@gmail.com>
2024-08-28 08:10:31 -0600

    Implement entering text in filter form

--- app/src/androidTest/java/bbct/android/ui/BaseballCardFilterScreenTest.kt
+++ app/src/androidTest/java/bbct/android/ui/BaseballCardFilterScreenTest.kt
@@ -2,11 +2,15 @@ package bbct.android.ui
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
 import androidx.navigation.compose.rememberNavController
 import androidx.room.Room.inMemoryDatabaseBuilder
 import androidx.test.platform.app.InstrumentationRegistry
 import bbct.android.data.BaseballCardDatabase
+import bbct.android.data.cards
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -49,5 +53,65 @@ class BaseballCardFilterScreenTest {
         composeTestRule
             .onNodeWithText("Team")
             .assertIsDisplayed()
+    }
+
+    @Test
+    fun testFilterByBrand() {
+        composeTestRule
+            .onNodeWithText("Brand")
+            .assertIsDisplayed()
+            .performTextInput(cards[0].brand)
+        composeTestRule
+            .onNodeWithContentDescription("Filter Cards")
+            .assertIsDisplayed()
+            .performClick()
+    }
+
+    @Test
+    fun testFilterByYear() {
+        composeTestRule
+            .onNodeWithText("Year")
+            .assertIsDisplayed()
+            .performTextInput(cards[0].year.toString())
+        composeTestRule
+            .onNodeWithContentDescription("Filter Cards")
+            .assertIsDisplayed()
+            .performClick()
+    }
+
+    @Test
+    fun testFilterByNumber() {
+        composeTestRule
+            .onNodeWithText("Number")
+            .assertIsDisplayed()
+            .performTextInput(cards[0].number)
+        composeTestRule
+            .onNodeWithContentDescription("Filter Cards")
+            .assertIsDisplayed()
+            .performClick()
+    }
+
+    @Test
+    fun testFilterByPlayerName() {
+        composeTestRule
+            .onNodeWithText("Player Name")
+            .assertIsDisplayed()
+            .performTextInput(cards[0].playerName)
+        composeTestRule
+            .onNodeWithContentDescription("Filter Cards")
+            .assertIsDisplayed()
+            .performClick()
+    }
+
+    @Test
+    fun testFilterByTeam() {
+        composeTestRule
+            .onNodeWithText("Team")
+            .assertIsDisplayed()
+            .performTextInput(cards[0].team)
+        composeTestRule
+            .onNodeWithContentDescription("Filter Cards")
+            .assertIsDisplayed()
+            .performClick()
     }
 }
--- app/src/main/java/bbct/android/ui/BaseballCardFilterScreen.kt
+++ app/src/main/java/bbct/android/ui/BaseballCardFilterScreen.kt
@@ -13,11 +13,23 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import bbct.android.R
 import bbct.android.data.BaseballCardDatabase
+
+
+data class BaseballCardFilterState(
+    var brand: String = "",
+    var year: String = "",
+    var number: String = "",
+    var playerName: String = "",
+    var team: String = ""
+)
 
 @Composable
 fun BaseballCardFilterScreen(
@@ -34,12 +46,19 @@ fun BaseballCardFilterScreen(
         floatingActionButton = { ApplyFilterButton(navController) },
         modifier = Modifier.fillMaxSize()
     ) { innerPadding ->
-        BaseballCardFilter(modifier = Modifier.padding(innerPadding))
+        val filterState = remember { mutableStateOf(BaseballCardFilterState()) }
+        BaseballCardFilter(
+            filterState,
+            modifier = Modifier.padding(innerPadding)
+        )
     }
 }
 
 @Composable
-fun BaseballCardFilter(modifier: Modifier = Modifier) {
+fun BaseballCardFilter(
+    filterState: MutableState<BaseballCardFilterState>,
+    modifier: Modifier = Modifier
+) {
     Column(modifier = modifier) {
         Row {
             Checkbox(
@@ -47,8 +66,8 @@ fun BaseballCardFilter(modifier: Modifier = Modifier) {
                 onCheckedChange = { /*TODO*/ })
             TextField(
                 label = { Text(text = stringResource(id = R.string.brand)) },
-                value = "",
-                onValueChange = { /*TODO*/ })
+                value = filterState.value.brand,
+                onValueChange = { filterState.value = filterState.value.copy(brand = it) })
         }
         Row {
             Checkbox(
@@ -56,8 +75,8 @@ fun BaseballCardFilter(modifier: Modifier = Modifier) {
                 onCheckedChange = { /*TODO*/ })
             TextField(
                 label = { Text(text = stringResource(id = R.string.year)) },
-                value = "",
-                onValueChange = { /*TODO*/ })
+                value = filterState.value.year,
+                onValueChange = { filterState.value = filterState.value.copy(year = it) })
         }
         Row {
             Checkbox(
@@ -65,8 +84,8 @@ fun BaseballCardFilter(modifier: Modifier = Modifier) {
                 onCheckedChange = { /*TODO*/ })
             TextField(
                 label = { Text(text = stringResource(id = R.string.number)) },
-                value = "",
-                onValueChange = { /*TODO*/ })
+                value = filterState.value.number,
+                onValueChange = { filterState.value = filterState.value.copy(number = it) })
         }
         Row {
             Checkbox(
@@ -74,8 +93,8 @@ fun BaseballCardFilter(modifier: Modifier = Modifier) {
                 onCheckedChange = { /*TODO*/ })
             TextField(
                 label = { Text(text = stringResource(id = R.string.player_name)) },
-                value = "",
-                onValueChange = { /*TODO*/ })
+                value = filterState.value.playerName,
+                onValueChange = { filterState.value = filterState.value.copy(playerName = it) })
         }
         Row {
             Checkbox(
@@ -83,8 +102,8 @@ fun BaseballCardFilter(modifier: Modifier = Modifier) {
                 onCheckedChange = { /*TODO*/ })
             TextField(
                 label = { Text(text = stringResource(id = R.string.team)) },
-                value = "",
-                onValueChange = { /*TODO*/ })
+                value = filterState.value.team,
+                onValueChange = { filterState.value = filterState.value.copy(team = it) })
         }
     }
 }
