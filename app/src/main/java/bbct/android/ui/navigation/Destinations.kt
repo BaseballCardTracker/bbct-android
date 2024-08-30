ad96a820657a68ef813eac672f3e1ef86f3cae01
Code-Apprentice <codeguru42@gmail.com>
2024-08-30 09:59:05 -0600

    Formatting

--- app/src/main/java/bbct/android/ui/navigation/Destinations.kt
+++ app/src/main/java/bbct/android/ui/navigation/Destinations.kt
@@ -11,24 +11,22 @@ import bbct.android.ui.BaseballCardListScreen
 
 object BaseballCardListDestination {
     const val route = "baseball_card_list"
-    val screen: @Composable (NavController, BaseballCardDatabase) -> Unit =
-        { navController, db ->
-            BaseballCardListScreen(
-                navController,
-                db,
-            )
-        }
+    val screen: @Composable (NavController, BaseballCardDatabase) -> Unit = { navController, db ->
+        BaseballCardListScreen(
+            navController,
+            db,
+        )
+    }
 }
 
 object BaseballCardCreateDestination {
     const val route = "baseball_card_details"
-    val screen: @Composable (NavController, BaseballCardDatabase) -> Unit =
-        { navController, db ->
-            BaseballCardCreateScreen(
-                navController,
-                db,
-            )
-        }
+    val screen: @Composable (NavController, BaseballCardDatabase) -> Unit = { navController, db ->
+        BaseballCardCreateScreen(
+            navController,
+            db,
+        )
+    }
 }
 
 object BaseballCardEditDestination {
@@ -50,22 +48,20 @@ fun NavController.navigate(cardId: Long) {
 
 object BaseballCardFilterDestination {
     const val route = "baseball_card_filter"
-    val screen: @Composable (NavController, BaseballCardDatabase) -> Unit =
-        { navController, db ->
-            BaseballCardFilterScreen(
-                navController,
-                db,
-            )
-        }
+    val screen: @Composable (NavController, BaseballCardDatabase) -> Unit = { navController, db ->
+        BaseballCardFilterScreen(
+            navController,
+            db,
+        )
+    }
 }
 
 object AboutDestination {
     const val route = "about"
-    val screen: @Composable (NavController, BaseballCardDatabase) -> Unit =
-        { navController, db ->
-            AboutScreen(
-                navController,
-                db,
-            )
-        }
+    val screen: @Composable (NavController, BaseballCardDatabase) -> Unit = { navController, db ->
+        AboutScreen(
+            navController,
+            db,
+        )
+    }
 }
