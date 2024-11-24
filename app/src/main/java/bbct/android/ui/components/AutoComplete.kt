608a9763de9a5964f5fcf6cced6e89a3acc56cee
Code-Apprentice <codeguru42@gmail.com>
2024-11-24 16:28:56 -0700

    Go back to a version of AutoComplete that uses ExposedDropdownMenu

--- app/src/main/java/bbct/android/ui/components/AutoComplete.kt
+++ app/src/main/java/bbct/android/ui/components/AutoComplete.kt
@@ -1,9 +1,10 @@
 package bbct.android.ui.components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -13,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AutoComplete(
     label: @Composable () -> Unit,
@@ -26,7 +28,9 @@ fun AutoComplete(
     var expanded by remember { mutableStateOf(false) }
     var filteredOpts by remember { mutableStateOf(options) }
 
-    Column {
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded }) {
         TextField(
             label = label,
             value = value,
@@ -40,15 +44,14 @@ fun AutoComplete(
                 expanded = true
             },
             isError = isError,
+            colors = ExposedDropdownMenuDefaults.textFieldColors(),
+            modifier = modifier.menuAnchor(),
             keyboardOptions = keyboardOptions,
-            modifier = modifier
         )
-
-        if (expanded && filteredOpts.isNotEmpty()) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth(),
-            ) {
+        if (!filteredOpts.isEmpty()) {
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }) {
                 filteredOpts.forEach { option ->
                     DropdownMenuItem(
                         text = { Text(text = option) },
