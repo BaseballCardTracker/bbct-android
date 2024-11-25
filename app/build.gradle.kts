29d2b270022e350b04a0b7139dbd5dfeadd6bee3
Code-Apprentice <codeguru42@gmail.com>
2024-11-24 19:06:03 -0700

    Fix serialization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.google.devtools.ksp)
     id("androidx.navigation.safeargs.kotlin")
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 android {
