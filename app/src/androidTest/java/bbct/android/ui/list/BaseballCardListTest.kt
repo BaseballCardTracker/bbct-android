ba5ef2acafd59addc329e88e662ccfde07c130b9
Code-Apprentice <codeguru42@gmail.com>
2024-11-30 09:26:08 -0700

    Use CardListTestRule

--- app/src/androidTest/java/bbct/android/ui/list/BaseballCardListTest.kt
+++ app/src/androidTest/java/bbct/android/ui/list/BaseballCardListTest.kt
@@ -8,7 +8,7 @@ import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.navigation.compose.rememberNavController
-import bbct.android.data.cards
+import bbct.android.rules.CardListTestRule
 import bbct.android.test.hasRole
 import org.junit.Before
 import org.junit.Rule
@@ -18,11 +18,21 @@ class BaseballCardListTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
+    @get:Rule
+    val cardListRule = CardListTestRule("three_cards.csv")
+
     private var cardListState = mutableListOf<BaseballCardSelectedState>()
 
     @Before
     fun setup() {
-        cardListState = cards.map { BaseballCardSelectedState(it, false) }.toMutableList()
+        cardListState = cardListRule.cards!!
+            .map {
+                BaseballCardSelectedState(
+                    it,
+                    false
+                )
+            }
+            .toMutableList()
         composeTestRule.setContent {
             val navController = rememberNavController()
             BaseballCardList(
@@ -36,11 +46,17 @@ class BaseballCardListTest {
 
     @Test
     fun testDisplaysCards() {
-        for ((i, card) in cards.withIndex()) {
+        for ((i, card) in cardListRule.cards!!.withIndex()) {
             composeTestRule.onAllNodesWithText(card.brand)[i].assertIsDisplayed()
-            composeTestRule.onNodeWithText("${card.year}").assertIsDisplayed()
-            composeTestRule.onNodeWithText(card.number).assertIsDisplayed()
-            composeTestRule.onNodeWithText(card.playerName).assertIsDisplayed()
+            composeTestRule
+                .onNodeWithText("${card.year}")
+                .assertIsDisplayed()
+            composeTestRule
+                .onNodeWithText(card.number)
+                .assertIsDisplayed()
+            composeTestRule
+                .onNodeWithText(card.playerName)
+                .assertIsDisplayed()
         }
     }
 
