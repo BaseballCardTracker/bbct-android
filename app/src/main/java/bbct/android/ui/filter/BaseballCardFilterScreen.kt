ae03ffc9a26c6e845458bf27c0a52d663692c23c
Code-Apprentice <codeguru42@gmail.com>
2024-12-16 21:28:23 -0700

    Remove checkboxes from filter screen

--- app/src/main/java/bbct/android/ui/filter/BaseballCardFilterScreen.kt
+++ app/src/main/java/bbct/android/ui/filter/BaseballCardFilterScreen.kt
@@ -1,7 +1,6 @@
 package bbct.android.ui.filter
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,7 +9,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
-import androidx.compose.material3.Checkbox
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -68,73 +66,48 @@ fun BaseballCardFilter(
 ) {
     val textFieldModifier = Modifier.fillMaxWidth()
 
-    Column(modifier = modifier) {
-        Row {
-            Checkbox(
-                checked = false,
-                onCheckedChange = { /*TODO*/ })
-            TextField(
-                label = { Text(text = stringResource(id = R.string.brand)) },
-                value = filterState.value.brand,
-                onValueChange = { filterState.value = filterState.value.copy(brand = it) },
-                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
-                modifier = textFieldModifier,
-            )
-        }
-        Row {
-            Checkbox(
-                checked = false,
-                onCheckedChange = { /*TODO*/ })
-            TextField(
-                label = { Text(text = stringResource(id = R.string.year)) },
-                value = if (filterState.value.year == -1) "" else filterState.value.year.toString(),
-                onValueChange = {
-                    val year = if (it == "") -1 else it.toInt()
-                    filterState.value = filterState.value.copy(year = year)
-                },
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Next,
-                    keyboardType = KeyboardType.Number
-                ),
-                modifier = textFieldModifier,
-            )
-        }
-        Row {
-            Checkbox(
-                checked = false,
-                onCheckedChange = { /*TODO*/ })
-            TextField(
-                label = { Text(text = stringResource(id = R.string.number)) },
-                value = filterState.value.number,
-                onValueChange = { filterState.value = filterState.value.copy(number = it) },
-                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
-                modifier = textFieldModifier,
-            )
-        }
-        Row {
-            Checkbox(
-                checked = false,
-                onCheckedChange = { /*TODO*/ })
-            TextField(
-                label = { Text(text = stringResource(id = R.string.player_name)) },
-                value = filterState.value.playerName,
-                onValueChange = { filterState.value = filterState.value.copy(playerName = it) },
-                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
-                modifier = textFieldModifier,
-            )
-        }
-        Row {
-            Checkbox(
-                checked = false,
-                onCheckedChange = { /*TODO*/ })
-            TextField(
-                label = { Text(text = stringResource(id = R.string.team)) },
-                value = filterState.value.team,
-                onValueChange = { filterState.value = filterState.value.copy(team = it) },
-                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
-                modifier = textFieldModifier,
-            )
-        }
+    Column(modifier = modifier.padding(12.dp)) {
+        TextField(
+            label = { Text(text = stringResource(id = R.string.brand)) },
+            value = filterState.value.brand,
+            onValueChange = { filterState.value = filterState.value.copy(brand = it) },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            modifier = textFieldModifier,
+        )
+        TextField(
+            label = { Text(text = stringResource(id = R.string.year)) },
+            value = if (filterState.value.year == -1) "" else filterState.value.year.toString(),
+            onValueChange = {
+                val year = if (it == "") -1 else it.toInt()
+                filterState.value = filterState.value.copy(year = year)
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Next,
+                keyboardType = KeyboardType.Number
+            ),
+            modifier = textFieldModifier,
+        )
+        TextField(
+            label = { Text(text = stringResource(id = R.string.number)) },
+            value = filterState.value.number,
+            onValueChange = { filterState.value = filterState.value.copy(number = it) },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            modifier = textFieldModifier,
+        )
+        TextField(
+            label = { Text(text = stringResource(id = R.string.player_name)) },
+            value = filterState.value.playerName,
+            onValueChange = { filterState.value = filterState.value.copy(playerName = it) },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            modifier = textFieldModifier,
+        )
+        TextField(
+            label = { Text(text = stringResource(id = R.string.team)) },
+            value = filterState.value.team,
+            onValueChange = { filterState.value = filterState.value.copy(team = it) },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            modifier = textFieldModifier,
+        )
     }
 }
 
