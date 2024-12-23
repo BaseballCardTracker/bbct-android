448258ad80d6a6bfabf0c7a23ee74937c45dc3d7
Code-Apprentice <codeguru42@gmail.com>
2024-12-23 13:18:26 -0700

    Delete original Java implementation

--- android/src/androidTestLite/java/bbct/android/lite/activity/LiteActivityNavigateUpFromBaseballCardDetailsTest.java
+++
@@ -1,9 +0,0 @@
-package bbct.android.lite.activity;
-
-import bbct.android.common.navigation.NavigateUpFromBaseballCardDetailsTest;
-
-public class LiteActivityNavigateUpFromBaseballCardDetailsTest extends NavigateUpFromBaseballCardDetailsTest<LiteActivity> {
-    public LiteActivityNavigateUpFromBaseballCardDetailsTest() {
-        super(LiteActivity.class);
-    }
-}
--- android/src/androidTestLite/java/bbct/android/lite/activity/LiteActivityNavigateUpFromFilterCardsTest.java
+++
@@ -1,9 +0,0 @@
-package bbct.android.lite.activity;
-
-import bbct.android.common.navigation.NavigateUpFromFilterCardsTest;
-
-public class LiteActivityNavigateUpFromFilterCardsTest extends NavigateUpFromFilterCardsTest<LiteActivity> {
-    public LiteActivityNavigateUpFromFilterCardsTest() {
-        super(LiteActivity.class);
-    }
-}
