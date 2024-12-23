2cc83e62f4f0d3ff81270c3b134f0998396406cf
Code-Apprentice <codeguru42@gmail.com>
2024-12-23 13:31:22 -0700

    Update versionCode and versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "bbct.android"
         minSdk = 24
         targetSdk = 35
-        versionCode = 25
-        versionName = "1.1.1"
+        versionCode = 26
+        versionName = "2025.01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
